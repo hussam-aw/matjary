8be2999df7ac0dd90d22ffff2c48d5a1b75e92eb
AliAlsaeed176 <alialsaeed176@gmail.com>
2023-03-13 04:35:21 -0700

    Create UI text styles and add app text styles to it

---
+++ lib/Constants/ui_text_styles.dart
@@ -0,0 +1,83 @@
+import 'package:flutter/material.dart';
+import 'package:matjary/Constants/ui_colors.dart';
+
+class UITextStyle {
+  static const boldLarge = TextStyle(
+    color: Colors.white,
+    fontFamily: 'Montserrat',
+    height: 1,
+    fontWeight: FontWeight.bold,
+    fontSize: 25.0,
+  );
+
+  static const normalLarge = TextStyle(
+    color: Colors.white,
+    fontFamily: 'Montserrat',
+    height: 1,
+    fontWeight: FontWeight.w400,
+    fontSize: 25.0,
+  );
+  static const boldHeading = TextStyle(
+    color: Colors.white,
+    fontFamily: 'Cairo',
+    height: 1,
+    fontWeight: FontWeight.bold,
+    fontSize: 20.0,
+  );
+
+  static const normalHeading = TextStyle(
+    color: Colors.white,
+    fontFamily: 'Cairo',
+    height: 1,
+    fontWeight: FontWeight.w400,
+    fontSize: 20.0,
+  );
+
+  static const boldMeduim = TextStyle(
+    color: Colors.white,
+    fontFamily: 'Cairo',
+    height: 1,
+    fontWeight: FontWeight.bold,
+    fontSize: 16.0,
+  );
+
+  static const normalMeduim = TextStyle(
+    color: Colors.white,
+    fontFamily: 'Cairo',
+    height: 1,
+    fontWeight: FontWeight.w400,
+    fontSize: 16.0,
+  );
+
+  static const boldBody = TextStyle(
+    color: Colors.white,
+    fontFamily: 'Cairo',
+    height: 1,
+    fontWeight: FontWeight.bold,
+    fontSize: 13.0,
+  );
+
+  static const normalBody = TextStyle(
+    color: Colors.white,
+    fontFamily: 'Cairo',
+    height: 1,
+    fontWeight: FontWeight.w400,
+    fontSize: 13.0,
+  );
+
+  static const boldSmall = TextStyle(
+    color: Colors.white,
+    fontFamily: 'Cairo',
+    height: 1,
+    fontWeight: FontWeight.bold,
+    fontSize: 10.0,
+  );
+
+  static const normalSmall = TextStyle(
+    color: Colors.white,
+    height: 1,
+    fontFamily: 'Cairo',
+    fontWeight: FontWeight.w400,
+    fontSize: 10.0,
+  );
+}
