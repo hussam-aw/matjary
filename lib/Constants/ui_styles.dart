c8182d4fc718c63da29cb81414511bb18e514fc6
AliAlsaeed176 <alialsaeed176@gmail.com>
2023-03-13 04:34:31 -0700

    Add elevated buttons and inputs styles

---
+++ lib/Constants/ui_styles.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'package:matjary/Constants/ui_colors.dart';
+import 'package:matjary/Constants/ui_text_styles.dart';
+
+// ElevatedButton Styles
+const acceptButtonStyle = ButtonStyle(
+  elevation: MaterialStatePropertyAll(0),
+  backgroundColor: MaterialStatePropertyAll<Color>(UIColors.primary),
+  shape: MaterialStatePropertyAll<RoundedRectangleBorder>(
+    RoundedRectangleBorder(
+      borderRadius: raduis15,
+    ),
+  ),
+  minimumSize: MaterialStatePropertyAll<Size>(
+    Size(double.infinity, 44),
+  ),
+);
+
+final acceptButtonWithBorderStyle = acceptButtonStyle.copyWith(
+  backgroundColor:
+      const MaterialStatePropertyAll<Color>(UIColors.mainBackground),
+  shape: const MaterialStatePropertyAll<RoundedRectangleBorder>(
+    RoundedRectangleBorder(
+      borderRadius: raduis15,
+      side: BorderSide(width: 1, color: UIColors.white),
+    ),
+  ),
+);
+
+// Input Styles
+final authInputStyle = InputDecoration(
+  filled: true,
+  fillColor: UIColors.textFieldBackground,
+  contentPadding: const EdgeInsets.symmetric(horizontal: 30, vertical: 20),
+  border: OutlineInputBorder(
+    borderRadius: BorderRadius.circular(15),
+  ),
+  errorBorder: OutlineInputBorder(
+    borderSide: const BorderSide(
+      width: 1,
+      color: UIColors.red,
+    ),
+    borderRadius: BorderRadius.circular(15),
+  ),
+  hintStyle: UITextStyle.normalBody,
+);
+
+// Border Radius
+const raduis15 = BorderRadius.all(Radius.circular(15));
+const radius19 = BorderRadius.all(Radius.circular(19));
+const raduis32top = BorderRadius.only(
+  topLeft: Radius.circular(32),
+  topRight: Radius.circular(32),
+);
+const raduis42top = BorderRadius.only(
+  topLeft: Radius.circular(42),
+  topRight: Radius.circular(42),
+);
