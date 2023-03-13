3b045eabd1c7aee0bc6a3212ad0118c617481d84
AliAlsaeed176 <alialsaeed176@gmail.com>
2023-03-13 04:34:06 -0700

    Create UI colors class and add UI colors

---
+++ lib/Constants/ui_colors.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class UIColors {
+  static const primary = Color(0xffFFAA00);
+  static const white = Color(0xffffffff);
+  static const red = Color(0xffff0000);
+  // Text colors
+  static const darkText = Color(0xff7D7D7D);
+  static const normalText = Color(0xffA4A4A4);
+  static const lightNormalText = Color(0xffF6F6F6);
+  // Icon Colors
+  static const normalIcon = Color(0xffC5C5C5);
+  // Border Colors
+  static const circleAvatarBorder = Color(0xff707070);
+  // Background Colors
+  static const mainBackground = Color(0xff1C1D40);
+  static const textFieldBackground = Color(0xff3D3E65);
+  static const buttonBackground = Color(0xff6C34F1);
+  static const containerBackground = Color(0xff3D3E65);
+  static const circleAvatarBackground = Color(0xff707070);
+}
