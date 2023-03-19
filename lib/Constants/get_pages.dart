4625dbb86b7fcd23fe311d279b61a7d9eb75ad50
AliAlsaeed176 <alialsaeed176@gmail.com>
2023-03-19 04:34:54 -0700

    Add login page

--- lib/Constants/get_pages.dart
+++ lib/Constants/get_pages.dart
@@ -1,7 +1,9 @@
 import 'package:get/get.dart';
+import 'package:matjary/PresentationLayer/Auth/login_screen.dart';
 import '../PresentationLayer/Public/splash_screen.dart';
 import 'get_routes.dart';
 
 List<GetPage<dynamic>> getPages = [
   GetPage(name: AppRoutes.splashscreen, page: () => const SplashScreen()),
+  GetPage(name: AppRoutes.loginscreen, page: () => const LoginScreen()),
 ];
