8e30775060b457b580cc42528e0da86f131770ff
AliAlsaeed176 <alialsaeed176@gmail.com>
2023-03-19 04:25:40 -0700

    Create splash screen

---
+++ lib/PresentationLayer/Public/splash_screen.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:matjary/Constants/ui_colors.dart';
+import 'package:matjary/PresentationLayer/Widgets/Public/primary_line.dart';
+
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: UIColors.mainBackground,
+      body: Center(
+        child: Image.asset(
+          'assets/images/logo.png',
+        ),
+      ),
+      bottomSheet: const PrimaryLine(),
+    );
+  }
+}
