dc6b21aed4acb936e3525abb0a8879ca23f3c087
AliAlsaeed176 <alialsaeed176@gmail.com>
2023-03-20 04:10:09 -0700

    Create custom app bar widget

---
+++ lib/PresentationLayer/Widgets/Public/custom_app_bar.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:matjary/Constants/ui_colors.dart';
+
+PreferredSize customAppBar({Widget? leading, List<Widget> actions = const []}) {
+  return PreferredSize(
+    preferredSize: Size(Get.width, 80),
+    child: AppBar(
+      elevation: 0.0,
+      backgroundColor: UIColors.mainBackground,
+      flexibleSpace: Align(
+        alignment: Alignment.centerLeft,
+        child: Padding(
+          padding: EdgeInsets.only(top: 20),
+          child: Image.asset(
+            'assets/images/logo.png',
+          ),
+        ),
+      ),
+      leading: Builder(
+        builder: (BuildContext context) {
+          return IconButton(
+            padding: const EdgeInsets.only(top: 15, right: 40),
+            icon: const Icon(
+              Icons.menu,
+              size: 35,
+            ),
+            onPressed: () {
+              Scaffold.of(context).openDrawer();
+            },
+          );
+        },
+      ),
+      actions: actions,
+    ),
+  );
+}
