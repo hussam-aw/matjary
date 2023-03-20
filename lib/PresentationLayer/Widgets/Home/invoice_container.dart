aef8e1b08af8c61cfe04286bcff7be93f8df769a
AliAlsaeed176 <alialsaeed176@gmail.com>
2023-03-20 04:11:10 -0700

    Create invoice container widget

---
+++ lib/PresentationLayer/Widgets/Home/invoice_container.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:matjary/Constants/ui_colors.dart';
+import 'package:matjary/Constants/ui_styles.dart';
+import 'package:matjary/Constants/ui_text_styles.dart';
+import 'package:matjary/PresentationLayer/Widgets/Public/spacerHeight.dart';
+
+class InovoiceContainer extends StatelessWidget {
+  InovoiceContainer({
+    super.key,
+    required this.invoiceType,
+    required this.invoiceAmount,
+    this.backgroundColor,
+  });
+
+  String invoiceType;
+  String invoiceAmount;
+  Color? backgroundColor;
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Container(
+        height: 100,
+        decoration: BoxDecoration(
+          color: backgroundColor ?? UIColors.containerBackground,
+          borderRadius: radius19,
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              invoiceType,
+              style: UITextStyle.normalSmall,
+            ),
+            spacerHeight(),
+            Text(
+              invoiceAmount,
+              style: UITextStyle.boldLarge,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
