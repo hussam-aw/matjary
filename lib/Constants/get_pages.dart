5eca60e08eee63cf390cf73ce9a14ec176152670
AliAlsaeed176 <alialsaeed176@gmail.com>
2023-03-21 00:36:46 -0700

    Add new pages

--- lib/Constants/get_pages.dart
+++ lib/Constants/get_pages.dart
@@ -1,9 +1,13 @@
 import 'package:get/get.dart';
 import 'package:matjary/PresentationLayer/Auth/login_screen.dart';
+import 'package:matjary/PresentationLayer/Auth/register_screen.dart';
+import 'package:matjary/PresentationLayer/Public/home_screen.dart';
 import '../PresentationLayer/Public/splash_screen.dart';
 import 'get_routes.dart';
 
 List<GetPage<dynamic>> getPages = [
   GetPage(name: AppRoutes.splashscreen, page: () => SplashScreen()),
   GetPage(name: AppRoutes.loginscreen, page: () => const LoginScreen()),
+  GetPage(name: AppRoutes.registerscreen, page: () => const RegisterScreen()),
+  GetPage(name: AppRoutes.homeScreen, page: () => const HomeScreen()),
 ];
