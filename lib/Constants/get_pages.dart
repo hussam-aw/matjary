92e0830b61f2af9bab5dcb7ea503f72199189524
AliAlsaeed176 <alialsaeed176@gmail.com>
2023-03-21 00:26:37 -0700

    Fix issue

--- lib/Constants/get_pages.dart
+++ lib/Constants/get_pages.dart
@@ -4,6 +4,6 @@ import '../PresentationLayer/Public/splash_screen.dart';
 import 'get_routes.dart';
 
 List<GetPage<dynamic>> getPages = [
-  GetPage(name: AppRoutes.splashscreen, page: () => const SplashScreen()),
+  GetPage(name: AppRoutes.splashscreen, page: () => SplashScreen()),
   GetPage(name: AppRoutes.loginscreen, page: () => const LoginScreen()),
 ];
