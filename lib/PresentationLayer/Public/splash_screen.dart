7e3f02621dab353bf06bce651e3ca3182cf8acec
AliAlsaeed176 <alialsaeed176@gmail.com>
2023-03-21 00:26:46 -0700

    Use splash controller

--- lib/PresentationLayer/Public/splash_screen.dart
+++ lib/PresentationLayer/Public/splash_screen.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:matjary/BussinessLayer/Controllers/splash_controller.dart';
 import 'package:matjary/Constants/ui_colors.dart';
 import 'package:matjary/PresentationLayer/Widgets/Public/primary_line.dart';
 
 class SplashScreen extends StatelessWidget {
-  const SplashScreen({super.key});
+  SplashScreen({super.key});
+
+  final SplashController splashController = Get.put(SplashController());
 
   @override
   Widget build(BuildContext context) {
