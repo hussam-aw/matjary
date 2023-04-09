b98e74dac99f44920b3e3043ace337c5f3c2281d
AliAlsaeed176 <alialsaeed176@gmail.com>
2023-04-09 05:56:12 -0700

    Rename screen

--- lib/PresentationLayer/Private/create_edit_category_screen.dart
+++ lib/PresentationLayer/Private/create_edit_category_screen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
-class CrateEditCategoryScreen extends StatelessWidget {
-  const CrateEditCategoryScreen({super.key});
+class CreateEditCategoryScreen extends StatelessWidget {
+  const CreateEditCategoryScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
