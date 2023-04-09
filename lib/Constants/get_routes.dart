1a9142e532063a597c53396ab70a04d892cdf19a
AliAlsaeed176 <alialsaeed176@gmail.com>
2023-04-09 05:55:37 -0700

    Add new routes

--- lib/Constants/get_routes.dart
+++ lib/Constants/get_routes.dart
@@ -5,4 +5,12 @@ class AppRoutes {
   static const loginscreen = "/login";
   static const registerscreen = "/register";
   static const homeScreen = "/home";
+  static const createEditAccountScreen = '/home/createEditAccountScreen';
+  static const createEditWareScreen = '/home/createEditWaveScreen';
+  static const createEditBankScreen = '/home/createEditBankScreen';
+  static const createEditClientScreen = '/home/createEditClientScreen';
+  static const createEditProductScreen = '/home/createEditProductScreen';
+  static const createEditCategoryScreen = '/home/createEditCategoryScreen';
+  static const createStatementScreen = '/home/createEditStatementScreen';
+  static const chooseAccountScreen = '/home/chooseAccountScreen';
 }
