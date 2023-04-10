5324794a2f2e90ff465f7bcdf19cb7eef2593982
AliAlsaeed176 <alialsaeed176@gmail.com>
2023-04-10 05:27:25 -0700

    Create choose account screen widget

--- lib/PresentationLayer/Private/choose_account_screen.dart
+++ lib/PresentationLayer/Private/choose_account_screen.dart
@@ -1,10 +1,57 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:matjary/Constants/ui_colors.dart';
+import 'package:matjary/Constants/ui_styles.dart';
+import 'package:matjary/Constants/ui_text_styles.dart';
+import 'package:matjary/PresentationLayer/Widgets/Private/account_box.dart';
+import 'package:matjary/PresentationLayer/Widgets/Public/custom_app_bar.dart';
+import 'package:matjary/PresentationLayer/Widgets/Public/custom_drawer.dart';
+import 'package:matjary/PresentationLayer/Widgets/Public/page_title.dart';
+import 'package:matjary/PresentationLayer/Widgets/Public/spacerHeight.dart';
 
 class ChooseAccountScreen extends StatelessWidget {
   const ChooseAccountScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Directionality(
+      textDirection: TextDirection.rtl,
+      child: Scaffold(
+        backgroundColor: UIColors.mainBackground,
+        appBar: customAppBar(showingAppIcon: false),
+        drawer: const CustomDrawer(),
+        body: SafeArea(
+          child: Container(
+            width: Get.width,
+            height: Get.height,
+            padding: const EdgeInsets.symmetric(vertical: 15, horizontal: 30),
+            child: Column(
+              children: [
+                const PageTitle(title: 'إختيار حساب'),
+                spacerHeight(),
+                TextFormField(
+                  textAlign: TextAlign.center,
+                  decoration: normalTextFieldStyle.copyWith(
+                    hintText: 'قم بالبحث عن اسم الحساب أو اختر من القائمة',
+                  ),
+                ),
+                spacerHeight(height: 20),
+                Expanded(
+                  child: ListView.separated(
+                    itemBuilder: (context, index) {
+                      return AccountBox(accountName: 'الزبون علي');
+                    },
+                    separatorBuilder: (context, index) {
+                      return spacerHeight();
+                    },
+                    itemCount: 20,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
