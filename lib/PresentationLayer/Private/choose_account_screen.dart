acd897418d21431ded4cb4645d226a821d06d60c
AliAlsaeed176 <alialsaeed176@gmail.com>
2023-04-10 02:27:16 -0700

    Fix some issues

--- lib/PresentationLayer/Private/choose_account_screen.dart
+++ lib/PresentationLayer/Private/choose_account_screen.dart
@@ -1,12 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:ionicons/ionicons.dart';
 import 'package:matjary/Constants/ui_colors.dart';
 import 'package:matjary/Constants/ui_styles.dart';
 import 'package:matjary/Constants/ui_text_styles.dart';
+import 'package:matjary/PresentationLayer/Widgets/Public/accept_button.dart';
 import 'package:matjary/PresentationLayer/Widgets/Public/custom_app_bar.dart';
 import 'package:matjary/PresentationLayer/Widgets/Public/custom_drawer.dart';
 import 'package:matjary/PresentationLayer/Widgets/Public/custom_dropdown_form_field.dart';
 import 'package:matjary/PresentationLayer/Widgets/Public/custom_radio_button.dart';
+import 'package:matjary/PresentationLayer/Widgets/Public/custom_radio_item.dart';
+import 'package:matjary/PresentationLayer/Widgets/Public/page_title.dart';
+import 'package:matjary/PresentationLayer/Widgets/Public/section_title.dart';
+import 'package:matjary/PresentationLayer/Widgets/Public/spacerHeight.dart';
 
 class ChooseAccountScreen extends StatelessWidget {
   const ChooseAccountScreen({super.key});
@@ -18,15 +24,89 @@ class ChooseAccountScreen extends StatelessWidget {
       child: Scaffold(
         backgroundColor: UIColors.mainBackground,
         appBar: customAppBar(showingAppIcon: false),
-        drawer: CustomDrawer(),
+        drawer: const CustomDrawer(),
         body: SafeArea(
           child: Container(
-            padding: EdgeInsets.all(30),
+            width: Get.width,
+            height: Get.height,
+            padding: const EdgeInsets.symmetric(vertical: 15, horizontal: 30),
             child: Column(
               children: [
-                CustomRadioButton(
-                  items: ['دائن', 'مدين'],
+                const PageTitle(title: 'إنشاء | تعديل حساب'),
+                Expanded(
+                  flex: 5,
+                  child: SingleChildScrollView(
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 20),
+                      child: Form(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            const SectionTitle(title: 'المعلومات الأساسية'),
+                            spacerHeight(),
+                            TextFormField(
+                              keyboardType: TextInputType.name,
+                              decoration: textFieldStyle.copyWith(
+                                hintText: 'اسم الحساب',
+                              ),
+                            ),
+                            spacerHeight(),
+                            TextFormField(
+                              keyboardType: TextInputType.number,
+                              decoration: textFieldStyle.copyWith(
+                                hintText: 'المبلغ الإبتدائي ( الجرد الأولي )',
+                              ),
+                            ),
+                            spacerHeight(height: 22),
+                            const SectionTitle(title: 'نمط الحساب'),
+                            spacerHeight(),
+                            CustomRadioButton(
+                              items: [
+                                RadioButtonItem(
+                                    text: 'مدين',
+                                    isSelected: true,
+                                    onTap: () {}),
+                                RadioButtonItem(text: 'دائن', onTap: () {})
+                              ],
+                            ),
+                            spacerHeight(height: 22),
+                            const SectionTitle(title: 'نوع الحساب'),
+                            spacerHeight(),
+                            const CustomDropdownFormField(
+                              items: ['حساب عادي', 'صندوق', 'جهة عمل'],
+                            ),
+                            spacerHeight(),
+                            TextFormField(
+                              keyboardType: TextInputType.emailAddress,
+                              decoration: textFieldStyle.copyWith(
+                                hintText: 'البريد الالكتروني',
+                              ),
+                            ),
+                            spacerHeight(),
+                            TextFormField(
+                              keyboardType: TextInputType.number,
+                              decoration: textFieldStyle.copyWith(
+                                hintText: 'الرقم',
+                              ),
+                            ),
+                            spacerHeight(),
+                            TextFormField(
+                              keyboardType: TextInputType.streetAddress,
+                              decoration: textFieldStyle.copyWith(
+                                hintText: 'العنوان',
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
                 ),
+                spacerHeight(height: 30),
+                AcceptButton(
+                  text: 'إنشاء',
+                  onPressed: () {},
+                )
               ],
             ),
           ),
--- lib/PresentationLayer/Private/create_edit_account_screen.dart
+++ lib/PresentationLayer/Private/create_edit_account_screen.dart
@@ -1,10 +1,115 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:matjary/Constants/ui_colors.dart';
+import 'package:matjary/Constants/ui_styles.dart';
+import 'package:matjary/PresentationLayer/Widgets/Public/accept_button.dart';
+import 'package:matjary/PresentationLayer/Widgets/Public/custom_app_bar.dart';
+import 'package:matjary/PresentationLayer/Widgets/Public/custom_drawer.dart';
+import 'package:matjary/PresentationLayer/Widgets/Public/custom_dropdown_form_field.dart';
+import 'package:matjary/PresentationLayer/Widgets/Public/custom_radio_button.dart';
+import 'package:matjary/PresentationLayer/Widgets/Public/custom_radio_item.dart';
+import 'package:matjary/PresentationLayer/Widgets/Public/page_title.dart';
+import 'package:matjary/PresentationLayer/Widgets/Public/section_title.dart';
+import 'package:matjary/PresentationLayer/Widgets/Public/spacerHeight.dart';
 
 class CreateEditAccountScreen extends StatelessWidget {
   const CreateEditAccountScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Directionality(
+      textDirection: TextDirection.rtl,
+      child: Scaffold(
+        backgroundColor: UIColors.mainBackground,
+        appBar: customAppBar(showingAppIcon: false),
+        drawer: const CustomDrawer(),
+        body: SafeArea(
+          child: Container(
+            width: Get.width,
+            height: Get.height,
+            padding: const EdgeInsets.symmetric(vertical: 15, horizontal: 30),
+            child: Column(
+              children: [
+                const PageTitle(title: 'إنشاء | تعديل حساب'),
+                Expanded(
+                  flex: 5,
+                  child: SingleChildScrollView(
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 20),
+                      child: Form(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            const SectionTitle(title: 'المعلومات الأساسية'),
+                            spacerHeight(),
+                            TextFormField(
+                              keyboardType: TextInputType.name,
+                              decoration: textFieldStyle.copyWith(
+                                hintText: 'اسم الحساب',
+                              ),
+                            ),
+                            spacerHeight(),
+                            TextFormField(
+                              keyboardType: TextInputType.number,
+                              decoration: textFieldStyle.copyWith(
+                                hintText: 'المبلغ الإبتدائي ( الجرد الأولي )',
+                              ),
+                            ),
+                            spacerHeight(height: 22),
+                            const SectionTitle(title: 'نمط الحساب'),
+                            spacerHeight(),
+                            CustomRadioButton(
+                              items: [
+                                RadioButtonItem(
+                                    text: 'مدين',
+                                    isSelected: true,
+                                    onTap: () {}),
+                                RadioButtonItem(text: 'دائن', onTap: () {})
+                              ],
+                            ),
+                            spacerHeight(height: 22),
+                            const SectionTitle(title: 'نوع الحساب'),
+                            spacerHeight(),
+                            const CustomDropdownFormField(
+                              items: ['حساب عادي', 'صندوق', 'جهة عمل'],
+                            ),
+                            spacerHeight(),
+                            TextFormField(
+                              keyboardType: TextInputType.emailAddress,
+                              decoration: textFieldStyle.copyWith(
+                                hintText: 'البريد الالكتروني',
+                              ),
+                            ),
+                            spacerHeight(),
+                            TextFormField(
+                              keyboardType: TextInputType.number,
+                              decoration: textFieldStyle.copyWith(
+                                hintText: 'الرقم',
+                              ),
+                            ),
+                            spacerHeight(),
+                            TextFormField(
+                              keyboardType: TextInputType.streetAddress,
+                              decoration: textFieldStyle.copyWith(
+                                hintText: 'العنوان',
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                spacerHeight(height: 30),
+                AcceptButton(
+                  text: 'إنشاء',
+                  onPressed: () {},
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
