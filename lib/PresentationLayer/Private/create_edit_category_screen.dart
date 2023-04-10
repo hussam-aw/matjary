9631336bf7889b7eca3e222126d02b5fda7beb1e
AliAlsaeed176 <alialsaeed176@gmail.com>
2023-04-10 04:46:22 -0700

    Create create edit cateory screen

--- lib/PresentationLayer/Private/create_edit_category_screen.dart
+++ lib/PresentationLayer/Private/create_edit_category_screen.dart
@@ -1,10 +1,62 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:matjary/Constants/ui_colors.dart';
+import 'package:matjary/Constants/ui_styles.dart';
+import 'package:matjary/PresentationLayer/Widgets/Public/accept_button.dart';
+import 'package:matjary/PresentationLayer/Widgets/Public/custom_app_bar.dart';
+import 'package:matjary/PresentationLayer/Widgets/Public/custom_drawer.dart';
+import 'package:matjary/PresentationLayer/Widgets/Public/page_title.dart';
+import 'package:matjary/PresentationLayer/Widgets/Public/section_title.dart';
+import 'package:matjary/PresentationLayer/Widgets/Public/spacerHeight.dart';
 
 class CreateEditCategoryScreen extends StatelessWidget {
   const CreateEditCategoryScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Directionality(
+      textDirection: TextDirection.rtl,
+      child: Scaffold(
+        backgroundColor: UIColors.mainBackground,
+        appBar: customAppBar(showingAppIcon: false),
+        drawer: const CustomDrawer(),
+        body: Container(
+          width: Get.width,
+          height: Get.height,
+          padding: const EdgeInsets.symmetric(vertical: 15, horizontal: 30),
+          child: Column(
+            children: [
+              const PageTitle(title: 'إنشاء | تعديل تصنيف بضاعة'),
+              Expanded(
+                flex: 5,
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 20),
+                  child: Form(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        const SectionTitle(title: 'المعلومات الأساسية'),
+                        spacerHeight(),
+                        TextFormField(
+                          keyboardType: TextInputType.name,
+                          decoration: textFieldStyle.copyWith(
+                            hintText: 'اسم التصنيف',
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+              spacerHeight(height: 30),
+              AcceptButton(
+                text: 'إنشاء',
+                onPressed: () {},
+              )
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
