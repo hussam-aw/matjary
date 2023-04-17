2df605c864865223b3c720fd8166c40a0829ae24
AliAlsaeed176 <alialsaeed176@gmail.com>
2023-04-17 00:25:27 -0700

    Merge changes

--- lib/Constants/api_links.dart
+++ lib/Constants/api_links.dart
@@ -1,7 +1,4 @@
 const baseUrl = "http://matjary2.brain.sy/api/v1";
 const loginLink = "/login";
-<<<<<<< HEAD
 const accountsLink = "/accounts/1";
-=======
 const createWare = "/ware";
->>>>>>> cc9c4a76334fd43b6805d886ab306a776fa2eafb
