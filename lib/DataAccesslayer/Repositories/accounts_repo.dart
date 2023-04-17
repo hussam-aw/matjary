40e71b69c463b55876428b544bc243aada81673f
AliAlsaeed176 <alialsaeed176@gmail.com>
2023-04-17 08:12:43 -0700

    Add create account to repo

--- lib/DataAccesslayer/Repositories/accounts_repo.dart
+++ lib/DataAccesslayer/Repositories/accounts_repo.dart
@@ -2,9 +2,12 @@ import 'dart:convert';
 
 import 'package:matjary/DataAccesslayer/Clients/accounts_client.dart';
 import 'package:matjary/DataAccesslayer/Models/account.dart';
+import 'package:matjary/DataAccesslayer/Models/bank_account.dart';
+import 'package:matjary/DataAccesslayer/Models/client_account.dart';
 
 class AccountsRepo {
   AccountsClient client = AccountsClient();
+
   Future<List<Account>> getAccounts() async {
     var response = await client.getAccounts();
     if (response != "") {
@@ -13,5 +16,40 @@ class AccountsRepo {
       return parsed.map<Account>((json) => Account.fromMap(json)).toList();
     }
     return [];
+  }
+
+  Future<List<Account>> getBankAccounts() async {
+    var response = await client.getBankAccounts();
+    if (response != "") {
+      final parsed = json.decode(response).cast<Map<String, dynamic>>();
+
+      return parsed
+          .map<BankAccount>((json) => BankAccount.fromMap(json))
+          .toList();
+    }
+    return [];
+  }
+
+  Future<List<Account>> getClientAccounts() async {
+    var response = await client.getClientAccounts();
+    if (response != "") {
+      final parsed = json.decode(response).cast<Map<String, dynamic>>();
+
+      return parsed
+          .map<ClientAccount>((json) => ClientAccount.fromMap(json))
+          .toList();
+    }
+    return [];
+  }
+
+  Future<Account?> createAccount(
+      name, balance, type, style, email, address, mobileNumber) async {
+    var createdAccount = await client.createAccount(
+        name, balance, type, style, email, address, mobileNumber);
+    print(createdAccount);
+    if (createdAccount != null) {
+      return Account.fromMap(jsonDecode(createdAccount));
+    }
+    return null;
   }
 }
