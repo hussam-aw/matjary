bb188e29928b2123782d1a6fca40186cf7dd2bd8
AliAlsaeed176 <alialsaeed176@gmail.com>
2023-04-17 08:10:14 -0700

    Create bank accounts screen

---
+++ lib/PresentationLayer/Private/choose_bank_account_screen.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:matjary/BussinessLayer/Controllers/account_controller.dart';
+import 'package:matjary/Constants/ui_colors.dart';
+import 'package:matjary/Constants/ui_styles.dart';
+import 'package:matjary/Constants/ui_text_styles.dart';
+import 'package:matjary/PresentationLayer/Widgets/Private/account_box.dart';
+import 'package:matjary/PresentationLayer/Widgets/Public/custom_app_bar.dart';
+import 'package:matjary/PresentationLayer/Widgets/Public/custom_drawer.dart';
+import 'package:matjary/PresentationLayer/Widgets/Public/page_title.dart';
+import 'package:matjary/PresentationLayer/Widgets/Public/spacerHeight.dart';
+
+class ChooseBankAccountScreen extends StatelessWidget {
+  ChooseBankAccountScreen({super.key});
+
+  final AccountController accountController = Get.put(AccountController());
+
+  @override
+  Widget build(BuildContext context) {
+    accountController.getBankAccounts();
+    return Directionality(
+      textDirection: TextDirection.rtl,
+      child: Scaffold(
+        backgroundColor: UIColors.mainBackground,
+        appBar: customAppBar(showingAppIcon: false),
+        drawer: const CustomDrawer(),
+        body: SafeArea(
+          child: Container(
+            padding: const EdgeInsets.symmetric(vertical: 15, horizontal: 30),
+            child: Column(
+              children: [
+                const PageTitle(title: 'إختيار حساب صندوق'),
+                spacerHeight(),
+                TextFormField(
+                  controller: TextEditingController(),
+                  textAlign: TextAlign.center,
+                  decoration: normalTextFieldStyle.copyWith(
+                    hintText: 'قم بالبحث عن اسم الحساب أو اختر من القائمة',
+                  ),
+                ),
+                spacerHeight(height: 20),
+                Expanded(
+                  child: Obx(
+                    () => accountController.isLoadingAccounts.value
+                        ? const Center(
+                            child: CircularProgressIndicator(),
+                          )
+                        : ListView.separated(
+                            itemBuilder: (context, index) {
+                              return AccountBox(
+                                  accountName: accountController
+                                      .bankAccounts[index].name);
+                            },
+                            separatorBuilder: (context, index) {
+                              return spacerHeight();
+                            },
+                            itemCount: accountController.bankAccounts.length,
+                          ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
