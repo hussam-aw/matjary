2f854be8fe7f914f9fd034d979cb8429fa8b5487
AliAlsaeed176 <alialsaeed176@gmail.com>
2023-04-18 03:49:05 -0700

    Create set type method

--- lib/BussinessLayer/Controllers/account_screen_controller.dart
+++ lib/BussinessLayer/Controllers/account_screen_controller.dart
@@ -3,14 +3,19 @@ import 'package:get/get_state_manager/get_state_manager.dart';
 class AccountScreenController extends GetxController {
   bool creditor = false;
   bool debtor = true;
-  void changeAccountType(type) {
-    if (type == 0) {
+
+  void setAccountType(type) {
+    if (type == 'مدين') {
       creditor = false;
       debtor = true;
     } else {
       creditor = true;
       debtor = false;
     }
+  }
+
+  void changeAccountType(type) {
+    setAccountType(type);
     update();
   }
 }
