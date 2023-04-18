e4fdab871d0387f819074bdeb3e4453457194c3b
AliAlsaeed176 <alialsaeed176@gmail.com>
2023-04-18 05:58:56 -0700

    Add background color parameter

--- lib/PresentationLayer/Widgets/Public/accept_button.dart
+++ lib/PresentationLayer/Widgets/Public/accept_button.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:matjary/Constants/ui_colors.dart';
 import 'package:matjary/Constants/ui_styles.dart';
 import 'package:matjary/Constants/ui_text_styles.dart';
 
@@ -7,18 +8,22 @@ class AcceptButton extends StatelessWidget {
     super.key,
     required this.text,
     this.style = acceptButtonStyle,
+    this.backgroundColor = UIColors.primary,
     required this.onPressed,
   });
 
   final String text;
   final ButtonStyle style;
+  final Color backgroundColor;
   final Function() onPressed;
 
   @override
   Widget build(BuildContext context) {
     return ElevatedButton(
       onPressed: onPressed,
-      style: style,
+      style: style.copyWith(
+        backgroundColor: MaterialStatePropertyAll<Color>(backgroundColor),
+      ),
       child: Text(
         text,
         style: UITextStyle.boldMeduim,
