c396a5a61d75586dbbf4d071457c83d3da4e4fb7
AliAlsaeed176 <alialsaeed176@gmail.com>
2023-04-26 05:28:58 -0700

    Add get orders, wares and products methods

--- lib/BussinessLayer/Controllers/home_controller.dart
+++ lib/BussinessLayer/Controllers/home_controller.dart
@@ -1,13 +1,26 @@
 import 'package:get/get.dart';
 import 'package:matjary/DataAccesslayer/Models/account.dart';
+import 'package:matjary/DataAccesslayer/Models/order.dart';
+import 'package:matjary/DataAccesslayer/Models/product.dart';
+import 'package:matjary/DataAccesslayer/Models/ware.dart';
 import 'package:matjary/DataAccesslayer/Repositories/accounts_repo.dart';
+import 'package:matjary/DataAccesslayer/Repositories/orders_repo.dart';
+import 'package:matjary/DataAccesslayer/Repositories/products_repo.dart';
+import 'package:matjary/DataAccesslayer/Repositories/ware_repo.dart';
 
 class HomeController extends GetxController {
   AccountsRepo accountsRepo = AccountsRepo();
+  WareRepo wareRepo = WareRepo();
+  OrdersRepo orderRepo = OrdersRepo();
+  PrdouctsRepo prdouctsRepo = PrdouctsRepo();
   List<Account> accounts = [];
   List<Account> bankAccounts = [];
   List<Account> clientAccounts = [];
+  List<Ware> wares = [];
+  List<Order> orders = [];
+  List<Product> products = [];
   var isLoadingAccounts = false.obs;
+
   Future<void> getAccounts() async {
     accounts = await accountsRepo.getAccounts();
     update();
@@ -25,11 +38,29 @@ class HomeController extends GetxController {
     isLoadingAccounts.value = false;
   }
 
+  Future<void> getWares() async {
+    wares = await wareRepo.getWares();
+    update();
+  }
+
+  Future<void> getOrders() async {
+    orders = await orderRepo.getOrders();
+    update();
+  }
+
+  Future<void> getProducts() async {
+    products = await prdouctsRepo.getProducts();
+    update();
+  }
+
   @override
   void onInit() {
     getAccounts();
     getBankAccounts();
     getClintAccounts();
+    getWares();
+    getOrders();
+    getProducts();
     super.onInit();
   }
 }
