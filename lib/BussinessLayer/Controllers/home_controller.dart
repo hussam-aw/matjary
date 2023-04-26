f906dd68d3d902f6265c13daaae28f1a9d197db6
AliAlsaeed176 <alialsaeed176@gmail.com>
2023-04-26 01:37:14 -0700

    Add get accounts methods

--- lib/BussinessLayer/Controllers/home_controller.dart
+++ lib/BussinessLayer/Controllers/home_controller.dart
@@ -1,3 +1,35 @@
 import 'package:get/get.dart';
+import 'package:matjary/DataAccesslayer/Models/account.dart';
+import 'package:matjary/DataAccesslayer/Repositories/accounts_repo.dart';
 
-class HomeController extends GetxController {}
+class HomeController extends GetxController {
+  AccountsRepo accountsRepo = AccountsRepo();
+  List<Account> accounts = [];
+  List<Account> bankAccounts = [];
+  List<Account> clientAccounts = [];
+  var isLoadingAccounts = false.obs;
+  Future<void> getAccounts() async {
+    accounts = await accountsRepo.getAccounts();
+    update();
+  }
+
+  Future<void> getBankAccounts() async {
+    isLoadingAccounts.value = true;
+    bankAccounts = await accountsRepo.getBankAccounts();
+    isLoadingAccounts.value = false;
+  }
+
+  Future<void> getClintAccounts() async {
+    isLoadingAccounts.value = true;
+    clientAccounts = await accountsRepo.getClientAccounts();
+    isLoadingAccounts.value = false;
+  }
+
+  @override
+  void onInit() {
+    getAccounts();
+    getBankAccounts();
+    getClintAccounts();
+    super.onInit();
+  }
+}
