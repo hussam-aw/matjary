19189728b41f886af830fe483df6a7f2c5897abb
AliAlsaeed176 <alialsaeed176@gmail.com>
2023-04-26 05:29:21 -0700

    Create custom dialog widget

---
+++ lib/PresentationLayer/Widgets/Public/custom_dialog.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:matjary/Constants/ui_colors.dart';
+import 'package:matjary/PresentationLayer/Widgets/Public/accept_button.dart';
+
+class CustomDialog extends StatelessWidget {
+  const CustomDialog({
+    super.key,
+    required this.title,
+    required this.buttonText,
+    this.backgroundColor = UIColors.red,
+    required this.confirmOnPressed,
+  });
+
+  final String title;
+  final String buttonText;
+  final Color backgroundColor;
+  final Function() confirmOnPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      title: Text(title),
+      actions: [
+        AcceptButton(
+          text: buttonText,
+          backgroundColor: backgroundColor,
+          onPressed: confirmOnPressed,
+        )
+      ],
+    );
+  }
+}
