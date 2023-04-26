06213a9583d0f52cde3165d5ed6b635a7f9baa62
AliAlsaeed176 <alialsaeed176@gmail.com>
2023-04-26 05:26:48 -0700

    Add get wares method

--- lib/DataAccesslayer/Repositories/ware_repo.dart
+++ lib/DataAccesslayer/Repositories/ware_repo.dart
@@ -5,6 +5,16 @@ import '../Models/ware.dart';
 
 class WareRepo {
   var client = WareClient();
+
+  Future<List<Ware>> getWares() async {
+    var response = await client.getWares();
+    if (response != "") {
+      final parsed = json.decode(response).cast<Map<String, dynamic>>();
+      return parsed.map<Ware>((json) => Ware.fromMap(json)).toList();
+    }
+    return [];
+  }
+
   Future<Ware?> postWare(name, userId) async {
     var data = await client.postWare(name, userId);
 
