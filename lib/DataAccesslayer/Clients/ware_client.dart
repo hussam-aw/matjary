75e3224e29309457d406de795895310492bb1de4
AliAlsaeed176 <alialsaeed176@gmail.com>
2023-04-26 05:25:49 -0700

    Add get wares method

--- lib/DataAccesslayer/Clients/ware_client.dart
+++ lib/DataAccesslayer/Clients/ware_client.dart
@@ -2,10 +2,20 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 import '../../Constants/api_links.dart';
 
-class WareClient{
+class WareClient {
+  Future<dynamic> getWares() async {
+    var response = await http.get(Uri.parse("$baseUrl$waresLink"));
+
+    if (response.statusCode == 200) {
+      return response.body;
+    } else {
+      return "";
+    }
+  }
+
   Future<dynamic> postWare(name, userId) async {
     var response = await http.post(Uri.parse("$baseUrl$createWare"),
-    //var response = await http.post(Uri.parse("http://matjary2.brain.sy/api/v1/ware"),
+        //var response = await http.post(Uri.parse("http://matjary2.brain.sy/api/v1/ware"),
         body: jsonEncode(<String, String>{
           "name": name,
           "user_id": userId.toString(),
@@ -13,8 +23,8 @@ class WareClient{
         headers: <String, String>{
           'Content-Type': 'application/json; charset=UTF-8',
         });
-print(response.statusCode);
-print(response.body);
+    print(response.statusCode);
+    print(response.body);
     if (response.statusCode == 201) {
       return response.body;
     } else {
