3cf6abaef6f0218a137d98f74f52170a7dd7a5ca
AliAlsaeed176 <alialsaeed176@gmail.com>
2023-04-27 01:28:03 -0700

    Create category model

---
+++ lib/DataAccesslayer/Models/category.dart
@@ -0,0 +1,27 @@
+class Category {
+  final int id;
+  final String name;
+  final String parent;
+
+  Category({
+    required this.id,
+    required this.name,
+    required this.parent,
+  });
+
+  Map<String, dynamic> toMap() {
+    return <String, dynamic>{
+      'id': id,
+      'name': name,
+      'parent': parent,
+    };
+  }
+
+  factory Category.fromMap(Map<String, dynamic> map) {
+    return Category(
+      id: map['id'] as int,
+      name: map['name'] as String,
+      parent: map['parent'] as String,
+    );
+  }
+}
