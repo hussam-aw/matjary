47515ff43231ed25674b515cac777fd381af012f
AliAlsaeed176 <alialsaeed176@gmail.com>
2023-04-27 05:26:53 -0700

    Add set product details

--- lib/BussinessLayer/Controllers/product_controller.dart
+++ lib/BussinessLayer/Controllers/product_controller.dart
@@ -1,3 +1,44 @@
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:matjary/DataAccesslayer/Models/product.dart';
 
-class ProductController extends GetxController {}
+class ProductController extends GetxController {
+  TextEditingController nameController = TextEditingController();
+  TextEditingController modelNumberController = TextEditingController();
+  TextEditingController initialPriceController = TextEditingController();
+  String affectedExchangeState = "يتأثر";
+  TextEditingController retailPriceController = TextEditingController();
+  TextEditingController supplierPriceController = TextEditingController();
+  TextEditingController wholesalePriceController = TextEditingController();
+
+  String convertAffectedExchangeStateToString(int state) {
+    if (state == 0) {
+      return 'لا يتأثر';
+    }
+    return 'يتأثر';
+  }
+
+  void setProductDetails(Product? product) {
+    if (product != null) {
+      nameController = TextEditingController(text: product.name);
+      modelNumberController =
+          TextEditingController(text: product.specialNumber);
+      initialPriceController =
+          TextEditingController(text: product.initialPrice.toString());
+      affectedExchangeState =
+          convertAffectedExchangeStateToString(product.affectedExchange);
+      wholesalePriceController =
+          TextEditingController(text: product.wholesalePrice.toString());
+      supplierPriceController =
+          TextEditingController(text: product.supplierPrice.toString());
+      retailPriceController =
+          TextEditingController(text: product.retailPrice.toString());
+    }
+  }
+
+  @override
+  void onInit() {
+    // TODO: implement onInit
+    super.onInit();
+  }
+}
