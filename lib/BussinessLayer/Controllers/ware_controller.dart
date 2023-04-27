aacfd09d30fc968f4c209f8aaa04f0dbb1ee55bb
AliAlsaeed176 <alialsaeed176@gmail.com>
2023-04-27 01:27:11 -0700

    Add update and delete ware methods

--- lib/BussinessLayer/Controllers/ware_controller.dart
+++ lib/BussinessLayer/Controllers/ware_controller.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:get/get.dart';
+import 'package:matjary/BussinessLayer/Controllers/home_controller.dart';
 
 import '../../DataAccesslayer/Models/ware.dart';
 import '../../DataAccesslayer/Repositories/ware_repo.dart';
@@ -7,10 +8,17 @@ import '../../PresentationLayer/Widgets/snackbars.dart';
 import '../../main.dart';
 
 class WareController extends GetxController {
+  TextEditingController nameOfWareController = TextEditingController();
   WareRepo wareRepo = WareRepo();
   List<Ware> wares = [];
   var adding = false.obs;
-  TextEditingController nameOfWareController = TextEditingController();
+  HomeController homeController = Get.find<HomeController>();
+
+  void setWareDetails(Ware? ware) {
+    if (ware != null) {
+      nameOfWareController = TextEditingController(text: ware.name);
+    }
+  }
 
   Future<void> addWare() async {
     adding.value = true;
@@ -22,6 +30,27 @@ class WareController extends GetxController {
       SnackBars.showSuccess('حدث خطأ أثناء الإضافة');
     } else {
       SnackBars.showSuccess('تمت إضافة مستودع جديد');
+    }
+  }
+
+  Future<void> updateWare(int id) async {
+    var account = await wareRepo.updateWare(id, nameOfWareController.text);
+    if (account != null) {
+      homeController.getWares();
+      SnackBars.showSuccess('تم التعديل بنجاح');
+    } else {
+      SnackBars.showError('فشل التعديل');
+    }
+  }
+
+  Future<void> deleteWare(id) async {
+    var ware = await wareRepo.deleteWare(id);
+    if (ware != null) {
+      homeController.getWares();
+      SnackBars.showSuccess('تم الحذف بنجاح');
+      update();
+    } else {
+      SnackBars.showError('فشل الحذف');
     }
   }
 }
