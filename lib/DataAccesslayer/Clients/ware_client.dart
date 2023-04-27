97445df8c6d05d618cc9e8f98073a39fa11e89ef
AliAlsaeed176 <alialsaeed176@gmail.com>
2023-04-27 01:27:46 -0700

    Add update and delete ware methods

--- lib/DataAccesslayer/Clients/ware_client.dart
+++ lib/DataAccesslayer/Clients/ware_client.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
+import 'package:matjary/main.dart';
 import '../../Constants/api_links.dart';
 
 class WareClient {
@@ -14,7 +15,7 @@ class WareClient {
   }
 
   Future<dynamic> postWare(name, userId) async {
-    var response = await http.post(Uri.parse("$baseUrl$createWare"),
+    var response = await http.post(Uri.parse("$baseUrl$wareLink"),
         //var response = await http.post(Uri.parse("http://matjary2.brain.sy/api/v1/ware"),
         body: jsonEncode(<String, String>{
           "name": name,
@@ -24,6 +25,35 @@ class WareClient {
           'Content-Type': 'application/json; charset=UTF-8',
         });
     print(response.statusCode);
+    print(response.body);
+    if (response.statusCode == 201) {
+      return response.body;
+    } else {
+      return null;
+    }
+  }
+
+  Future<dynamic> updateWare(id, name) async {
+    var response = await http.post(Uri.parse('$baseUrl$wareLink/$id'),
+        body: jsonEncode(<String, dynamic>{
+          "name": name,
+          "user_id": MyApp.appUser!.id,
+        }),
+        headers: <String, String>{
+          'Content-Type': 'application/json; charset=UTF-8',
+        });
+
+    print(response.body);
+    if (response.statusCode == 201) {
+      return response.body;
+    } else {
+      return null;
+    }
+  }
+
+  Future<dynamic> deleteWare(id) async {
+    var response = await http.delete(Uri.parse('$baseUrl$wareLink/$id'));
+
     print(response.body);
     if (response.statusCode == 201) {
       return response.body;
