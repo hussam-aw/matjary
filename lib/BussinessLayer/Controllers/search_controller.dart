9043befe3eb47d25cecab0d1b6d40248f5222a59
AliAlsaeed176 <alialsaeed176@gmail.com>
2023-04-29 02:21:50 -0700

    Create search controller

---
+++ lib/BussinessLayer/Controllers/search_controller.dart
@@ -0,0 +1,38 @@
+import 'package:get/state_manager.dart';
+
+import '../../DataAccesslayer/Models/product.dart';
+import '../../DataAccesslayer/Repositories/products_repo.dart';
+
+class SearchController extends GetxController {
+  List<dynamic> list = [];
+  List<dynamic> filteredList = [];
+  String searchText = "";
+  var searchLoading = false.obs;
+
+  void search() async {
+    if (searchText.isNotEmpty) {
+      searchLoading.value = true;
+      filteredList = list
+          .where((item) =>
+              item.name.toLowerCase().contains(searchText.toLowerCase()))
+          .toList();
+
+      print(filteredList);
+      searchLoading.value = false;
+    } else {
+      filteredList.clear();
+    }
+    update();
+  }
+
+  @override
+  void onInit() async {
+    super.onInit();
+  }
+
+  @override
+  void onClose() {
+    filteredList.clear();
+    super.onClose();
+  }
+}
