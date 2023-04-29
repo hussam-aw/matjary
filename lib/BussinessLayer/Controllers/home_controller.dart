e4e2706418e4c7146629a2e7b5316a8cbeca733d
AliAlsaeed176 <alialsaeed176@gmail.com>
2023-04-29 04:23:49 -0700

    Fix issue related to fetching data

--- lib/BussinessLayer/Controllers/home_controller.dart
+++ lib/BussinessLayer/Controllers/home_controller.dart
@@ -16,40 +16,58 @@ class HomeController extends GetxController {
   OrdersRepo orderRepo = OrdersRepo();
   PrdouctsRepo prdouctsRepo = PrdouctsRepo();
   CategoriesRepo categoriesRepo = CategoriesRepo();
+  var isLoading = false.obs;
   List<Account> accounts = [];
+  var isLoadingAccounts = false.obs;
   List<Account> bankAccounts = [];
   List<Account> clientAccounts = [];
   List<Ware> wares = [];
+  var isLoadingWares = false.obs;
   List<Order> orders = [];
+  var isLoadingOrders = false.obs;
   List<Order> purchasesOrders = [];
   List<Order> salesOrders = [];
   List<Product> products = [];
+  var isLoadingProducts = false.obs;
   List<Category> categories = [];
-  var isLoading = false.obs;
+  var isLoadingCategories = false.obs;
 
   Future<void> getAccounts() async {
+    isLoadingAccounts.value = true;
     accounts = await accountsRepo.getAccounts();
-    update();
+    isLoadingAccounts.value = false;
+    getBankAccounts();
+    getClientAccounts();
   }
 
-  Future<void> getBankAccounts() async {
-    bankAccounts = await accountsRepo.getBankAccounts();
+  void getBankAccounts() {
+    bankAccounts = accounts.where((account) => account.type == 0).toList();
   }
 
-  Future<void> getClientAccounts() async {
-    clientAccounts = await accountsRepo.getClientAccounts();
+  void getClientAccounts() {
+    clientAccounts = accounts.where((account) => account.type == 1).toList();
   }
+
+  // Future<void> getBankAccounts() async {
+  //   bankAccounts = await accountsRepo.getBankAccounts();
+  // }
+
+  // Future<void> getClientAccounts() async {
+  //   clientAccounts = await accountsRepo.getClientAccounts();
+  // }
 
   Future<void> getWares() async {
+    isLoadingWares.value = true;
     wares = await wareRepo.getWares();
-    update();
+    isLoadingWares.value = false;
   }
 
   Future<void> getOrders() async {
+    isLoadingOrders.value = true;
     orders = await orderRepo.getOrders();
+    isLoadingOrders.value = false;
     getPurchasesOrders();
     getSalesOrders();
-    update();
   }
 
   void getPurchasesOrders() {
@@ -61,24 +79,38 @@ class HomeController extends GetxController {
   }
 
   Future<void> getProducts() async {
+    isLoadingProducts.value = true;
     products = await prdouctsRepo.getProducts();
-    update();
+    isLoadingProducts.value = false;
   }
 
   Future<void> getCategories() async {
+    isLoadingCategories.value = true;
     categories = await categoriesRepo.getCategories();
+    isLoadingCategories.value = false;
+  }
+
+  void fetchData() async {
+    isLoading.value = true;
+    await getAccounts();
+    await getWares();
+    await getOrders();
+    await getProducts();
+    await getCategories();
+    isLoading.value = false;
     update();
   }
 
   @override
   void onInit() {
-    getAccounts();
-    getBankAccounts();
-    getClientAccounts();
-    getWares();
-    getOrders();
-    getProducts();
-    getCategories();
+    fetchData();
+    // getAccounts();
+    // getBankAccounts();
+    // getClientAccounts();
+    // getWares();
+    // getOrders();
+    // getProducts();
+    // getCategories();
     super.onInit();
   }
 }
