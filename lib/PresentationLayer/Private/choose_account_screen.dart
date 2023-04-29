d8b4bb3e3d965eacff52bf58ce2d5a679ae1c9b9
AliAlsaeed176 <alialsaeed176@gmail.com>
2023-04-29 02:23:34 -0700

    Add search account

--- lib/PresentationLayer/Private/choose_account_screen.dart
+++ lib/PresentationLayer/Private/choose_account_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:matjary/BussinessLayer/Controllers/account_controller.dart';
 import 'package:matjary/BussinessLayer/Controllers/home_controller.dart';
+import 'package:matjary/BussinessLayer/Controllers/search_controller.dart';
 import 'package:matjary/Constants/get_routes.dart';
 import 'package:matjary/Constants/ui_colors.dart';
 import 'package:matjary/Constants/ui_styles.dart';
@@ -15,11 +16,14 @@ import 'package:matjary/PresentationLayer/Widgets/Public/spacerHeight.dart';
 class ChooseAccountScreen extends StatelessWidget {
   ChooseAccountScreen({super.key});
 
+  TextEditingController accountNameController = TextEditingController();
   final AccountController accountController = Get.put(AccountController());
   final HomeController homeController = Get.find<HomeController>();
+  final SearchController searchController = Get.put(SearchController());
 
   @override
   Widget build(BuildContext context) {
+    searchController.list = homeController.accounts;
     return Directionality(
       textDirection: TextDirection.rtl,
       child: Scaffold(
@@ -39,6 +43,10 @@ class ChooseAccountScreen extends StatelessWidget {
                   decoration: normalTextFieldStyle.copyWith(
                     hintText: 'قم بالبحث عن اسم الحساب أو اختر من القائمة',
                   ),
+                  onChanged: (value) {
+                    searchController.searchText = value;
+                    searchController.search();
+                  },
                 ),
                 spacerHeight(height: 20),
                 Expanded(
@@ -48,29 +56,78 @@ class ChooseAccountScreen extends StatelessWidget {
                         ? Center(
                             child: loadingItem(width: 100, isWhite: true),
                           )
-                        : ListView.separated(
-                            itemBuilder: (context, index) {
-                              return CustomBox(
-                                title: homeController.accounts[index].name,
-                                editOnPressed: () {
-                                  Get.toNamed(AppRoutes.createEditAccountScreen,
-                                      arguments:
-                                          homeController.accounts[index]);
-                                },
-                                deleteDialogTitle:
-                                    'هل تريد بالتأكيد حذف الحساب؟',
-                                deleteOnPressed: () {
-                                  accountController.deleteAccount(
-                                      homeController.accounts[index].id);
-                                  Get.back();
-                                },
-                              );
-                            },
-                            separatorBuilder: (context, index) {
-                              return spacerHeight();
-                            },
-                            itemCount: homeController.accounts.length,
-                          ),
+                        : GetBuilder(
+                            init: searchController,
+                            builder: (context) {
+                              return searchController.searchText.isEmpty
+                                  ? ListView.separated(
+                                      itemBuilder: (context, index) {
+                                        return CustomBox(
+                                          title: homeController
+                                              .accounts[index].name,
+                                          editOnPressed: () {
+                                            Get.toNamed(
+                                                AppRoutes
+                                                    .createEditAccountScreen,
+                                                arguments: homeController
+                                                    .accounts[index]);
+                                          },
+                                          deleteDialogTitle:
+                                              'هل تريد بالتأكيد حذف الحساب؟',
+                                          deleteOnPressed: () {
+                                            accountController.deleteAccount(
+                                                homeController
+                                                    .accounts[index].id);
+                                            Get.back();
+                                          },
+                                        );
+                                      },
+                                      separatorBuilder: (context, index) {
+                                        return spacerHeight();
+                                      },
+                                      itemCount: homeController.accounts.length,
+                                    )
+                                  : Obx(() {
+                                      return searchController
+                                              .searchLoading.value
+                                          ? Center(
+                                              child: loadingItem(),
+                                            )
+                                          : ListView.separated(
+                                              itemBuilder: (context, index) {
+                                                return CustomBox(
+                                                  title: searchController
+                                                      .filteredList[index].name,
+                                                  editOnPressed: () {
+                                                    Get.toNamed(
+                                                        AppRoutes
+                                                            .createEditAccountScreen,
+                                                        arguments:
+                                                            homeController
+                                                                    .accounts[
+                                                                index]);
+                                                  },
+                                                  deleteDialogTitle:
+                                                      'هل تريد بالتأكيد حذف الحساب؟',
+                                                  deleteOnPressed: () {
+                                                    accountController
+                                                        .deleteAccount(
+                                                            homeController
+                                                                .accounts[index]
+                                                                .id);
+                                                    Get.back();
+                                                  },
+                                                );
+                                              },
+                                              separatorBuilder:
+                                                  (context, index) {
+                                                return spacerHeight();
+                                              },
+                                              itemCount: searchController
+                                                  .filteredList.length,
+                                            );
+                                    });
+                            }),
                   ),
                 ),
               ],
