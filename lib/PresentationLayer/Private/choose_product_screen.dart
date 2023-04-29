dc63bec944d905dba9b88c75fd1747fdedfbf0b0
AliAlsaeed176 <alialsaeed176@gmail.com>
2023-04-29 03:08:06 -0700

    Add products search

--- lib/PresentationLayer/Private/choose_product_screen.dart
+++ lib/PresentationLayer/Private/choose_product_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:matjary/BussinessLayer/Controllers/home_controller.dart';
+import 'package:matjary/BussinessLayer/Controllers/search_controller.dart';
 import 'package:matjary/Constants/get_routes.dart';
 import 'package:matjary/Constants/ui_colors.dart';
 import 'package:matjary/Constants/ui_styles.dart';
@@ -15,9 +16,11 @@ class ChooseProductScreen extends StatelessWidget {
   ChooseProductScreen({super.key});
 
   final HomeController homeController = Get.find<HomeController>();
+  final SearchController searchController = Get.put(SearchController());
 
   @override
   Widget build(BuildContext context) {
+    searchController.list = homeController.products;
     return Directionality(
       textDirection: TextDirection.rtl,
       child: Scaffold(
@@ -32,11 +35,14 @@ class ChooseProductScreen extends StatelessWidget {
                 const PageTitle(title: 'إختيار منتج'),
                 spacerHeight(),
                 TextFormField(
-                  controller: TextEditingController(),
                   textAlign: TextAlign.center,
                   decoration: normalTextFieldStyle.copyWith(
                     hintText: 'قم بالبحث عن اسم المنتج أو اختر من القائمة',
                   ),
+                  onChanged: (value) {
+                    searchController.searchText = value;
+                    searchController.search();
+                  },
                 ),
                 spacerHeight(height: 20),
                 Expanded(
@@ -46,29 +52,71 @@ class ChooseProductScreen extends StatelessWidget {
                         ? Center(
                             child: loadingItem(width: 100, isWhite: true),
                           )
-                        : ListView.separated(
-                            itemBuilder: (context, index) {
-                              return CustomBox(
-                                title: homeController.products[index].name,
-                                editOnPressed: () {
-                                  Get.toNamed(
-                                    AppRoutes.createEditProductScreen,
-                                    arguments: homeController.products[index],
-                                  );
-                                },
-                                deleteDialogTitle:
-                                    'هل تريد بالتأكيد حذف المنتج؟',
-                                deleteOnPressed: () {
-                                  //Delete from product controller
-                                  Get.back();
-                                },
-                              );
-                            },
-                            separatorBuilder: (context, index) {
-                              return spacerHeight();
-                            },
-                            itemCount: homeController.wares.length,
-                          ),
+                        : GetBuilder(
+                            init: searchController,
+                            builder: (context) {
+                              return searchController.searchText.isEmpty
+                                  ? ListView.separated(
+                                      itemBuilder: (context, index) {
+                                        return CustomBox(
+                                          title: homeController
+                                              .products[index].name,
+                                          editOnPressed: () {
+                                            Get.toNamed(
+                                              AppRoutes.createEditProductScreen,
+                                              arguments: homeController
+                                                  .products[index],
+                                            );
+                                          },
+                                          deleteDialogTitle:
+                                              'هل تريد بالتأكيد حذف المنتج؟',
+                                          deleteOnPressed: () {
+                                            //Delete from product controller
+                                            Get.back();
+                                          },
+                                        );
+                                      },
+                                      separatorBuilder: (context, index) {
+                                        return spacerHeight();
+                                      },
+                                      itemCount: homeController.wares.length,
+                                    )
+                                  : Obx(() {
+                                      return searchController
+                                              .searchLoading.value
+                                          ? Center(
+                                              child: loadingItem(),
+                                            )
+                                          : ListView.separated(
+                                              itemBuilder: (context, index) {
+                                                return CustomBox(
+                                                  title: searchController
+                                                      .filteredList[index].name,
+                                                  editOnPressed: () {
+                                                    Get.toNamed(
+                                                      AppRoutes
+                                                          .createEditProductScreen,
+                                                      arguments: homeController
+                                                          .products[index],
+                                                    );
+                                                  },
+                                                  deleteDialogTitle:
+                                                      'هل تريد بالتأكيد حذف الحساب؟',
+                                                  deleteOnPressed: () {
+                                                    //Delete from product controller
+                                                    Get.back();
+                                                  },
+                                                );
+                                              },
+                                              separatorBuilder:
+                                                  (context, index) {
+                                                return spacerHeight();
+                                              },
+                                              itemCount: searchController
+                                                  .filteredList.length,
+                                            );
+                                    });
+                            }),
                   ),
                 ),
               ],
