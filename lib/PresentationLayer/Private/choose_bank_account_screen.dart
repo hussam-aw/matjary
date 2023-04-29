ff0f8eb1fe3c9c91e81bdb4229bc0f4dd4a691d2
AliAlsaeed176 <alialsaeed176@gmail.com>
2023-04-29 04:36:46 -0700

    Add bank account search

--- lib/PresentationLayer/Private/choose_bank_account_screen.dart
+++ lib/PresentationLayer/Private/choose_bank_account_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:matjary/BussinessLayer/Controllers/account_controller.dart';
 import 'package:matjary/BussinessLayer/Controllers/home_controller.dart';
+import 'package:matjary/BussinessLayer/Controllers/search_controller.dart';
 import 'package:matjary/Constants/get_routes.dart';
 import 'package:matjary/Constants/ui_colors.dart';
 import 'package:matjary/Constants/ui_styles.dart';
@@ -9,6 +10,7 @@ import 'package:matjary/Constants/ui_text_styles.dart';
 import 'package:matjary/PresentationLayer/Widgets/Private/custom_box.dart';
 import 'package:matjary/PresentationLayer/Widgets/Public/custom_app_bar.dart';
 import 'package:matjary/PresentationLayer/Widgets/Public/custom_drawer.dart';
+import 'package:matjary/PresentationLayer/Widgets/Public/loading_item.dart';
 import 'package:matjary/PresentationLayer/Widgets/Public/page_title.dart';
 import 'package:matjary/PresentationLayer/Widgets/Public/spacerHeight.dart';
 
@@ -17,8 +19,11 @@ class ChooseBankAccountScreen extends StatelessWidget {
 
   final AccountController accountController = Get.put(AccountController());
   final homeController = Get.find<HomeController>();
+  final SearchController searchController = Get.put(SearchController());
+
   @override
   Widget build(BuildContext context) {
+    searchController.list = homeController.bankAccounts;
     return Directionality(
       textDirection: TextDirection.rtl,
       child: Scaffold(
@@ -33,43 +38,95 @@ class ChooseBankAccountScreen extends StatelessWidget {
                 const PageTitle(title: 'إختيار حساب صندوق'),
                 spacerHeight(),
                 TextFormField(
-                  controller: TextEditingController(),
                   textAlign: TextAlign.center,
                   decoration: normalTextFieldStyle.copyWith(
                     hintText: 'قم بالبحث عن اسم الحساب أو اختر من القائمة',
                   ),
+                  onChanged: (value) {
+                    searchController.searchText = value;
+                    searchController.search();
+                  },
                 ),
                 spacerHeight(height: 20),
                 Expanded(
-                  child: GetBuilder(
-                    init: homeController,
-                    builder: (context) => homeController.bankAccounts.isEmpty
-                        ? const Center(
-                            child: CircularProgressIndicator(),
+                  child: Obx(
+                    () => homeController.isLoadingAccounts.value
+                        ? Center(
+                            child: loadingItem(width: 100, isWhite: true),
                           )
-                        : ListView.separated(
-                            itemBuilder: (context, index) {
-                              return CustomBox(
-                                title: homeController.bankAccounts[index].name,
-                                editOnPressed: () {
-                                  Get.toNamed(AppRoutes.createEditAccountScreen,
-                                      arguments:
-                                          homeController.bankAccounts[index]);
-                                },
-                                deleteDialogTitle:
-                                    'هل تريد بالتأكيد حذف الحساب؟',
-                                deleteOnPressed: () {
-                                  accountController.deleteAccount(
-                                      homeController.bankAccounts[index].id);
-                                  Get.back();
-                                },
-                              );
-                            },
-                            separatorBuilder: (context, index) {
-                              return spacerHeight();
-                            },
-                            itemCount: homeController.bankAccounts.length,
-                          ),
+                        : GetBuilder(
+                            init: searchController,
+                            builder: (context) {
+                              return searchController.searchText.isEmpty
+                                  ? ListView.separated(
+                                      itemBuilder: (context, index) {
+                                        return CustomBox(
+                                          title: homeController
+                                              .bankAccounts[index].name,
+                                          editOnPressed: () {
+                                            Get.toNamed(
+                                                AppRoutes
+                                                    .createEditAccountScreen,
+                                                arguments: homeController
+                                                    .bankAccounts[index]);
+                                          },
+                                          deleteDialogTitle:
+                                              'هل تريد بالتأكيد حذف الحساب؟',
+                                          deleteOnPressed: () {
+                                            accountController.deleteAccount(
+                                                homeController
+                                                    .bankAccounts[index].id);
+                                            Get.back();
+                                          },
+                                        );
+                                      },
+                                      separatorBuilder: (context, index) {
+                                        return spacerHeight();
+                                      },
+                                      itemCount:
+                                          homeController.bankAccounts.length,
+                                    )
+                                  : Obx(() {
+                                      return searchController
+                                              .searchLoading.value
+                                          ? Center(
+                                              child: loadingItem(),
+                                            )
+                                          : ListView.separated(
+                                              itemBuilder: (context, index) {
+                                                return CustomBox(
+                                                  title: searchController
+                                                      .filteredList[index].name,
+                                                  editOnPressed: () {
+                                                    Get.toNamed(
+                                                        AppRoutes
+                                                            .createEditAccountScreen,
+                                                        arguments: homeController
+                                                                .bankAccounts[
+                                                            index]);
+                                                  },
+                                                  deleteDialogTitle:
+                                                      'هل تريد بالتأكيد حذف الحساب؟',
+                                                  deleteOnPressed: () {
+                                                    accountController
+                                                        .deleteAccount(
+                                                            homeController
+                                                                .bankAccounts[
+                                                                    index]
+                                                                .id);
+                                                    Get.back();
+                                                  },
+                                                );
+                                              },
+                                              separatorBuilder:
+                                                  (context, index) {
+                                                return spacerHeight();
+                                              },
+                                              itemCount: searchController
+                                                  .filteredList.length,
+                                            );
+                                    });
+                            }),
                   ),
                 ),
               ],
