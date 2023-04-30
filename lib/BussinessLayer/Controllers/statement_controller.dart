4fa853d69234aa65994a8b584b316c4928af80f0
AliAlsaeed176 <alialsaeed176@gmail.com>
2023-04-30 00:50:15 -0700

    Add some statement fields

--- lib/BussinessLayer/Controllers/statement_controller.dart
+++ lib/BussinessLayer/Controllers/statement_controller.dart
@@ -1,3 +1,19 @@
+import 'package:flutter/cupertino.dart';
 import 'package:get/get.dart';
 
-class StatementController extends GetxController {}
+class StatementController extends GetxController {
+  String fromAccount = "";
+  String toAccount = "";
+  TextEditingController statementAmountController =
+      TextEditingController(text: "0.0");
+
+  void setFromAccountInDropdownButton(accountName) {
+    fromAccount = accountName;
+    update();
+  }
+
+  void setToAccountInDropdownButton(accountName) {
+    toAccount = accountName;
+    update();
+  }
+}
