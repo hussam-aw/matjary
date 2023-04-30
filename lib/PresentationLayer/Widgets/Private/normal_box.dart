a6c375846adf9057392907d2dc7e758bd44b8f3f
AliAlsaeed176 <alialsaeed176@gmail.com>
2023-04-30 00:48:16 -0700

    Create clickable normal box

--- lib/PresentationLayer/Widgets/Private/normal_box.dart
+++ lib/PresentationLayer/Widgets/Private/normal_box.dart
@@ -1,10 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:matjary/Constants/ui_colors.dart';
+import 'package:matjary/Constants/ui_text_styles.dart';
 
 class NormalBox extends StatelessWidget {
-  const NormalBox({super.key});
+  const NormalBox({
+    super.key,
+    required this.title,
+    required this.onTap,
+  });
+
+  final String title;
+  final Function() onTap;
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return InkWell(
+      onTap: onTap,
+      child: Container(
+        height: 55,
+        padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 25),
+        decoration: const BoxDecoration(
+          color: UIColors.containerBackground,
+        ),
+        child: Text(
+          title,
+          textAlign: TextAlign.right,
+          overflow: TextOverflow.ellipsis,
+          style: UITextStyle.normalMeduim.copyWith(
+            color: UIColors.lightNormalText,
+          ),
+        ),
+      ),
+    );
   }
 }
