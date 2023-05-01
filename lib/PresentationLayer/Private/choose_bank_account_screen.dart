eca6f0c9637ef170dfa8e4f4f455b4037bc1ca96
AliAlsaeed176 <alialsaeed176@gmail.com>
2023-05-01 01:56:52 -0700

    Refactor screen code

--- lib/PresentationLayer/Private/choose_bank_account_screen.dart
+++ lib/PresentationLayer/Private/choose_bank_account_screen.dart
@@ -6,7 +6,6 @@ import 'package:matjary/BussinessLayer/Controllers/search_controller.dart';
 import 'package:matjary/Constants/get_routes.dart';
 import 'package:matjary/Constants/ui_colors.dart';
 import 'package:matjary/Constants/ui_styles.dart';
-import 'package:matjary/Constants/ui_text_styles.dart';
 import 'package:matjary/PresentationLayer/Widgets/Private/custom_box.dart';
 import 'package:matjary/PresentationLayer/Widgets/Public/custom_app_bar.dart';
 import 'package:matjary/PresentationLayer/Widgets/Public/custom_drawer.dart';
@@ -18,7 +17,7 @@ class ChooseBankAccountScreen extends StatelessWidget {
   ChooseBankAccountScreen({super.key});
 
   final AccountController accountController = Get.put(AccountController());
-  final homeController = Get.find<HomeController>();
+  final HomeController homeController = Get.find<HomeController>();
   final SearchController searchController = Get.put(SearchController());
 
   @override
@@ -35,12 +34,12 @@ class ChooseBankAccountScreen extends StatelessWidget {
             padding: const EdgeInsets.symmetric(vertical: 15, horizontal: 30),
             child: Column(
               children: [
-                const PageTitle(title: 'إختيار حساب صندوق'),
+                const PageTitle(title: 'إختيار صندوق'),
                 spacerHeight(),
                 TextFormField(
                   textAlign: TextAlign.center,
                   decoration: normalTextFieldStyle.copyWith(
-                    hintText: 'قم بالبحث عن اسم الحساب أو اختر من القائمة',
+                    hintText: 'قم بالبحث عن اسم الصندوق أو اختر من القائمة',
                   ),
                   onChanged: (value) {
                     searchController.searchText = value;
