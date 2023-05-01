d59034e5316f72bf5b6e325f44adbbed4ce470c7
AliAlsaeed176 <alialsaeed176@gmail.com>
2023-05-01 01:56:13 -0700

    Make getting clinet accounts dynamic

--- lib/PresentationLayer/Public/home_screen.dart
+++ lib/PresentationLayer/Public/home_screen.dart
@@ -32,155 +32,155 @@ class HomeScreen extends StatelessWidget {
         appBar: customAppBar(),
         drawer: CustomDrawer(),
         body: SafeArea(
-          child: GetBuilder(
-              init: homeController,
-              builder: (context) {
-                print(homeController.isLoading.value);
-                return homeController.isLoading.value
-                    ? Center(child: loadingItem(width: 100, isWhite: true))
-                    : Column(
-                        children: [
-                          Expanded(
-                            flex: 7,
-                            child: Padding(
-                              padding: const EdgeInsets.all(30),
-                              child: Column(
-                                children: [
-                                  Expanded(
-                                    child: Container(
-                                      width: width,
-                                      padding: const EdgeInsets.symmetric(
-                                          horizontal: 15, vertical: 4),
-                                      decoration: const BoxDecoration(
-                                        color: UIColors.containerBackground,
-                                        borderRadius: radius19,
-                                      ),
-                                      child: Row(
-                                        mainAxisSize: MainAxisSize.min,
-                                        mainAxisAlignment:
-                                            MainAxisAlignment.spaceBetween,
-                                        children: [
-                                          Expanded(
-                                            flex: 5,
-                                            child: Row(
-                                              children: [
-                                                const Icon(
-                                                  FontAwesomeIcons
-                                                      .moneyCheckDollar,
-                                                  color: UIColors.primary,
-                                                  size: 50,
-                                                ),
-                                                spacerWidth(),
-                                                Column(
-                                                  mainAxisAlignment:
-                                                      MainAxisAlignment.center,
-                                                  crossAxisAlignment:
-                                                      CrossAxisAlignment.start,
-                                                  children: [
-                                                    const Text(
-                                                      'رصيد الصندوق',
-                                                      style: UITextStyle
-                                                          .normalSmall,
-                                                    ),
-                                                    spacerHeight(height: 10),
-                                                    const Text(
-                                                      '1.500.000',
-                                                      style: UITextStyle
-                                                          .boldHeading,
-                                                    ),
-                                                  ],
-                                                ),
-                                              ],
+          child: Obx(() {
+            return homeController.isLoading.value
+                ? Center(child: loadingItem(width: 100, isWhite: true))
+                : Column(
+                    children: [
+                      Expanded(
+                        flex: 7,
+                        child: Padding(
+                          padding: const EdgeInsets.all(30),
+                          child: Column(
+                            children: [
+                              Expanded(
+                                child: Container(
+                                  width: width,
+                                  padding: const EdgeInsets.symmetric(
+                                      horizontal: 15, vertical: 4),
+                                  decoration: const BoxDecoration(
+                                    color: UIColors.containerBackground,
+                                    borderRadius: radius19,
+                                  ),
+                                  child: Row(
+                                    mainAxisSize: MainAxisSize.min,
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceBetween,
+                                    children: [
+                                      Expanded(
+                                        flex: 5,
+                                        child: Row(
+                                          children: [
+                                            const Icon(
+                                              FontAwesomeIcons.moneyCheckDollar,
+                                              color: UIColors.primary,
+                                              size: 50,
                                             ),
-                                          ),
-                                          Expanded(
-                                            flex: 2,
-                                            child: Row(
+                                            spacerWidth(),
+                                            Column(
                                               mainAxisAlignment:
                                                   MainAxisAlignment.center,
+                                              crossAxisAlignment:
+                                                  CrossAxisAlignment.start,
                                               children: [
-                                                OrderIconButton(
-                                                  onTap: () {},
-                                                  title: 'استلام',
-                                                  icon: FontAwesomeIcons
-                                                      .solidCircleDown,
+                                                const Text(
+                                                  'رصيد الصندوق',
+                                                  style:
+                                                      UITextStyle.normalSmall,
                                                 ),
-                                                spacerWidth(width: 10),
-                                                OrderIconButton(
-                                                  onTap: () {},
-                                                  title: 'ارسال',
-                                                  icon: FontAwesomeIcons
-                                                      .solidCircleUp,
-                                                )
+                                                spacerHeight(height: 10),
+                                                const Text(
+                                                  '1.500.000',
+                                                  style:
+                                                      UITextStyle.boldHeading,
+                                                ),
                                               ],
                                             ),
-                                          ),
-                                        ],
-                                      ),
-                                    ),
-                                  ),
-                                  spacerHeight(height: 20),
-                                  Expanded(
-                                    child: Row(
-                                      children: [
-                                        InovoiceContainer(
-                                          invoiceType: 'فواتير المشتريات',
-                                          invoiceAmount: homeController
-                                              .purchasesOrders.length
-                                              .toString(),
+                                          ],
                                         ),
-                                        spacerWidth(width: 40),
-                                        InovoiceContainer(
-                                          invoiceType: 'فواتير المبيعات',
-                                          invoiceAmount: homeController
-                                              .salesOrders.length
-                                              .toString(),
-                                          backgroundColor: UIColors.primary,
-                                        )
-                                      ],
+                                      ),
+                                      Expanded(
+                                        flex: 2,
+                                        child: Row(
+                                          mainAxisAlignment:
+                                              MainAxisAlignment.center,
+                                          children: [
+                                            OrderIconButton(
+                                              onTap: () {},
+                                              title: 'استلام',
+                                              icon: FontAwesomeIcons
+                                                  .solidCircleDown,
+                                            ),
+                                            spacerWidth(width: 10),
+                                            OrderIconButton(
+                                              onTap: () {},
+                                              title: 'ارسال',
+                                              icon: FontAwesomeIcons
+                                                  .solidCircleUp,
+                                            )
+                                          ],
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ),
+                              spacerHeight(height: 20),
+                              Expanded(
+                                child: Row(
+                                  children: [
+                                    InovoiceContainer(
+                                      invoiceType: 'فواتير المشتريات',
+                                      invoiceAmount: homeController
+                                          .purchasesOrders.length
+                                          .toString(),
                                     ),
-                                  ),
-                                ],
+                                    spacerWidth(width: 40),
+                                    InovoiceContainer(
+                                      invoiceType: 'فواتير المبيعات',
+                                      invoiceAmount: homeController
+                                          .salesOrders.length
+                                          .toString(),
+                                      backgroundColor: UIColors.primary,
+                                    )
+                                  ],
+                                ),
                               ),
-                            ),
+                            ],
                           ),
-                          Expanded(
-                            flex: 8,
-                            child: Container(
-                              width: Get.width,
-                              padding: const EdgeInsets.only(
-                                  top: 30, left: 35, right: 35),
-                              decoration: const BoxDecoration(
-                                color: UIColors.containerBackground,
-                                borderRadius: raduis32top,
+                        ),
+                      ),
+                      Expanded(
+                        flex: 8,
+                        child: Container(
+                          width: Get.width,
+                          padding: const EdgeInsets.only(
+                              top: 30, left: 35, right: 35),
+                          decoration: const BoxDecoration(
+                            color: UIColors.containerBackground,
+                            borderRadius: raduis32top,
+                          ),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              const Text(
+                                'حسابات العملاء',
+                                style: UITextStyle.boldBody,
                               ),
-                              child: Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  const Text(
-                                    'حسابات العملاء',
-                                    style: UITextStyle.boldBody,
-                                  ),
-                                  spacerHeight(height: 10),
-                                  Expanded(
-                                    child: ListView.separated(
-                                      itemBuilder: (context, index) {
-                                        return CustomerAccountListTile(
-                                          customerName: homeController
-                                              .clientAccounts[index].name,
-                                          customerImage:
-                                              'assets/images/user.png',
-                                          customerStatus: 'زبون',
-                                          customerBalance: homeController
-                                              .clientAccounts[index].balance
-                                              .toString(),
-                                        );
-                                      },
-                                      separatorBuilder: (context, index) {
-                                        return spacerHeight(height: 25);
-                                      },
-                                      itemCount:
-                                          homeController.clientAccounts.isEmpty
+                              spacerHeight(height: 10),
+                              Expanded(
+                                child: Obx(() {
+                                  return homeController
+                                          .isLoadingClientAccounts.value
+                                      ? Container()
+                                      : ListView.separated(
+                                          itemBuilder: (context, index) {
+                                            return CustomerAccountListTile(
+                                              customerName: homeController
+                                                  .clientAccounts[index].name,
+                                              customerImage:
+                                                  'assets/images/user.png',
+                                              customerStatus: 'زبون',
+                                              customerBalance: homeController
+                                                  .clientAccounts[index].balance
+                                                  .toString(),
+                                            );
+                                          },
+                                          separatorBuilder: (context, index) {
+                                            return spacerHeight(height: 25);
+                                          },
+                                          itemCount: homeController
+                                                  .clientAccounts.isEmpty
                                               ? 0
                                               : homeController.clientAccounts
                                                           .length <
@@ -188,15 +188,16 @@ class HomeScreen extends StatelessWidget {
                                                   ? homeController
                                                       .clientAccounts.length
                                                   : 5,
-                                    ),
-                                  ),
-                                ],
+                                        );
+                                }),
                               ),
-                            ),
+                            ],
                           ),
-                        ],
-                      );
-              }),
+                        ),
+                      ),
+                    ],
+                  );
+          }),
         ),
         bottomNavigationBar: const CustomBottomNavigationBar(),
         floatingActionButton: CustomIconButton(
