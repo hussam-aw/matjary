b58ed322fe9a1a0fda080bf9200e438fc08e3de7
AliAlsaeed176 <alialsaeed176@gmail.com>
2023-05-06 07:16:36 -0700

    Navigate to order details

--- lib/PresentationLayer/Private/Order/create_edit_order_screen.dart
+++ lib/PresentationLayer/Private/Order/create_edit_order_screen.dart
@@ -5,7 +5,8 @@ import 'package:matjary/BussinessLayer/Controllers/order_screen_controller.dart'
 import 'package:matjary/Constants/get_routes.dart';
 import 'package:matjary/Constants/ui_colors.dart';
 import 'package:matjary/Constants/ui_text_styles.dart';
-import 'package:matjary/PresentationLayer/Private/Order/order_basic_information_screen.dart';
+import 'package:matjary/PresentationLayer/Private/Order/order_basic_information.dart';
+import 'package:matjary/PresentationLayer/Private/Order/order_details.dart';
 import 'package:matjary/PresentationLayer/Widgets/Public/accept_button.dart';
 import 'package:matjary/PresentationLayer/Widgets/Public/custom_app_bar.dart';
 import 'package:matjary/PresentationLayer/Widgets/Public/custom_drawer.dart';
@@ -38,7 +39,7 @@ class CreateEditOrderScreen extends StatelessWidget {
               spacerHeight(),
               Expanded(
                 flex: 5,
-                child: OrderBasicInformationScreen(),
+                child: OrderDetails(),
               ),
               AcceptButton(
                 text: 'التالي',
