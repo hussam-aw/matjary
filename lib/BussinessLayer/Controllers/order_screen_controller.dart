e6a3683d7019d62dee8e7a6da46e5755e95a32a1
AliAlsaeed176 <alialsaeed176@gmail.com>
2023-05-08 06:49:40 -0700

    Make selection maps observable for using in radio buttons

--- lib/BussinessLayer/Controllers/order_screen_controller.dart
+++ lib/BussinessLayer/Controllers/order_screen_controller.dart
@@ -9,16 +9,54 @@ class OrderScreenController extends GetxController {
     'مردود شراء'
   ];
 
-  Map<String, bool> orderSelection = {
+  RxMap<String, bool> orderTypesSelection = {
     'بيع للزبائن': true,
     'بيع مفرق': false,
     'مشتريات': false,
     'مردود بيع': false,
     'مردود شراء': false,
-  };
+  }.obs;
+
+  List<String> buyingTypes = [
+    'مباشر',
+    'توصيل',
+    'شحن',
+  ];
+
+  RxMap<String, bool> buyingTypesSelection = {
+    'مباشر': true,
+    'توصيل': false,
+    'شحن': false,
+  }.obs;
+
+  List<String> orderStatus = [
+    'تامة',
+    'جاري التجهيز',
+    'تم التسديد',
+    'في شركة الشحن',
+    'قيد التوصيل',
+  ];
+
+  RxMap<String, bool> orderStatusSelection = {
+    'تامة': true,
+    'جاري التجهيز': false,
+    'تم التسديد': false,
+    'في شركة الشحن': false,
+    'قيد التوصيل': false,
+  }.obs;
+
+  List<String> discountTypes = [
+    'رقم',
+    'نسبة',
+  ];
+
+  RxMap<String, bool> discountTypesSelection = {
+    'رقم': true,
+    'نسبة': false,
+  }.obs;
 
   void resetOrderType() {
-    orderSelection = {
+    orderTypesSelection.value = {
       'بيع للزبائن': false,
       'بيع مفرق': false,
       'مشتريات': false,
@@ -27,13 +65,63 @@ class OrderScreenController extends GetxController {
     };
   }
 
-  void setOrderType(type) {
-    resetOrderType();
-    orderSelection[type] = true;
+  void resetbuyingType() {
+    buyingTypesSelection.value = {
+      'مباشر': false,
+      'توصيل': false,
+      'شحن': false,
+    };
   }
 
-  void changeOrderType(type) {
-    setOrderType(type);
-    update();
+  void resetOrderStatus() {
+    orderStatusSelection.value = {
+      'تامة': false,
+      'جاري التجهيز': false,
+      'تم التسديد': false,
+      'في شركة الشحن': false,
+      'قيد التوصيل': false,
+    };
   }
+
+  void resetDiscountType() {
+    discountTypesSelection.value = {
+      'رقم': false,
+      'نسبة': false,
+    };
+  }
+
+  void setOrderType(type) {
+    resetOrderType();
+    orderTypesSelection[type] = true;
+  }
+
+  void setbuyingType(type) {
+    resetbuyingType();
+    buyingTypesSelection[type] = true;
+  }
+
+  void setOrderStatus(type) {
+    resetOrderStatus();
+    orderStatusSelection[type] = true;
+  }
+
+  void setDiscountType(type) {
+    resetDiscountType();
+    discountTypesSelection[type] = true;
+  }
+
+  // void changeOrderType(type) {
+  //   setOrderType(type);
+  //   update();
+  // }
+
+  // void changebuyingType(type) {
+  //   setbuyingType(type);
+  //   update();
+  // }
+
+  // void changeOrderStatus(type) {
+  //   setOrderStatus(type);
+  //   update();
+  // }
 }
