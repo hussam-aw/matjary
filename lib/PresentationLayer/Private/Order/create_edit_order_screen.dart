78f4ac182dcce781e3c59fefb1a20a6a5556dcdb
AliAlsaeed176 <alialsaeed176@gmail.com>
2023-05-08 06:55:21 -0700

    Navigate to delivery details page

--- lib/PresentationLayer/Private/Order/create_edit_order_screen.dart
+++ lib/PresentationLayer/Private/Order/create_edit_order_screen.dart
@@ -5,6 +5,7 @@ import 'package:matjary/BussinessLayer/Controllers/order_screen_controller.dart'
 import 'package:matjary/Constants/get_routes.dart';
 import 'package:matjary/Constants/ui_colors.dart';
 import 'package:matjary/Constants/ui_text_styles.dart';
+import 'package:matjary/PresentationLayer/Private/Order/delivery_details.dart';
 import 'package:matjary/PresentationLayer/Private/Order/order_basic_information.dart';
 import 'package:matjary/PresentationLayer/Private/Order/order_details.dart';
 import 'package:matjary/PresentationLayer/Widgets/Public/accept_button.dart';
@@ -39,7 +40,7 @@ class CreateEditOrderScreen extends StatelessWidget {
               spacerHeight(),
               Expanded(
                 flex: 5,
-                child: OrderDetails(),
+                child: DeliveryDetails(),
               ),
               AcceptButton(
                 text: 'التالي',
