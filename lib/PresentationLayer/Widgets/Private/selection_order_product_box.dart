23d2b0329dfec4768dbfb914ffd10b1b7311616b
AliAlsaeed176 <alialsaeed176@gmail.com>
2023-05-10 07:45:48 -0700

    Make selection product box dynamic for entering quantity

--- lib/PresentationLayer/Widgets/Private/selection_order_product_box.dart
+++ lib/PresentationLayer/Widgets/Private/selection_order_product_box.dart
@@ -1,64 +1,130 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:matjary/BussinessLayer/Controllers/order_screen_controller.dart';
 import 'package:matjary/Constants/ui_colors.dart';
+import 'package:matjary/Constants/ui_styles.dart';
 import 'package:matjary/Constants/ui_text_styles.dart';
 import 'package:matjary/PresentationLayer/Widgets/Private/circle_text_button.dart';
+import 'package:matjary/PresentationLayer/Widgets/Public/accept_button.dart';
+import 'package:matjary/PresentationLayer/Widgets/Public/custom_text_form_field.dart';
+import 'package:matjary/PresentationLayer/Widgets/Public/spacerHeight.dart';
 import 'package:matjary/PresentationLayer/Widgets/Public/spacerWidth.dart';
+import 'package:matjary/PresentationLayer/Widgets/snackbars.dart';
 
 class SelectionOrderProductBox extends StatelessWidget {
-  const SelectionOrderProductBox({super.key});
+  SelectionOrderProductBox({
+    super.key,
+    required this.productId,
+    required this.productName,
+  });
+
+  final int productId;
+  final String productName;
+  final orderScreenController = Get.find<OrderScreenController>();
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 60,
-      color: UIColors.containerBackground,
-      padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 15),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          const Expanded(
-            flex: 2,
-            child: Text(
-              'باوربانك ريماكس RM-79',
-              maxLines: 2,
-              overflow: TextOverflow.ellipsis,
-              style: UITextStyle.normalBody,
-            ),
-          ),
-          Expanded(
-            flex: 2,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                CircleTextButton(
-                  text: '+',
-                  onTap: () {},
-                ),
-                spacerWidth(width: 5),
-                CircleTextButton(
-                  text: '-',
-                  onTap: () {},
-                ),
-                spacerWidth(width: 5),
-                CircleTextButton(
-                  text: '123',
-                  textStyle: UITextStyle.normalSmall,
-                  onTap: () {},
-                ),
-              ],
-            ),
-          ),
-          const Expanded(
-            flex: 1,
-            child: Align(
-              alignment: Alignment.center,
+    return Obx(() {
+      return Container(
+        height: 60,
+        color: orderScreenController.checkIfProductQuantityIsZero(productId)
+            ? UIColors.containerBackground
+            : UIColors.primary,
+        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 15),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Expanded(
+              flex: 2,
               child: Text(
-                '200',
-                style: UITextStyle.boldLarge,
+                productName,
+                maxLines: 2,
+                overflow: TextOverflow.ellipsis,
+                style: UITextStyle.normalBody,
               ),
             ),
+            Expanded(
+              flex: 2,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  CircleTextButton(
+                    text: '+',
+                    onTap: () {
+                      orderScreenController.increaseProductQuantity(productId);
+                    },
+                  ),
+                  spacerWidth(width: 5),
+                  CircleTextButton(
+                    text: '-',
+                    onTap: () {
+                      orderScreenController.decreaseProductQuantity(productId);
+                    },
+                  ),
+                  spacerWidth(width: 5),
+                  CircleTextButton(
+                    text: '123',
+                    textStyle: UITextStyle.normalSmall,
+                    onTap: () {
+                      showQuantityBottomSheet();
+                    },
+                  ),
+                ],
+              ),
+            ),
+            Expanded(
+              flex: 1,
+              child: Align(
+                alignment: Alignment.center,
+                child: Text(
+                  (orderScreenController
+                              .selectedProductsQuantities[productId] ??
+                          0)
+                      .toString(),
+                  style: UITextStyle.boldLarge,
+                ),
+              ),
+            ),
+          ],
+        ),
+      );
+    });
+  }
+
+  void showQuantityBottomSheet() {
+    Get.bottomSheet(
+      Container(
+        height: Get.width * .55,
+        decoration: const BoxDecoration(
+          borderRadius: raduis32top,
+          color: UIColors.white,
+        ),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 50, vertical: 40),
+          child: Column(
+            children: [
+              Text(
+                'يرجى ادخال الكمية للمنتج',
+                style:
+                    UITextStyle.normalMeduim.copyWith(color: UIColors.darkText),
+              ),
+              spacerHeight(),
+              TextFormField(
+                textAlign: TextAlign.center,
+                controller: orderScreenController.productQuantityController,
+                keyboardType: TextInputType.number,
+                decoration: normalTextFieldStyle,
+              ),
+              spacerHeight(height: 30),
+              AcceptButton(
+                text: 'تأكيد',
+                onPressed: () {
+                  orderScreenController.setProductQuantity(productId);
+                },
+              ),
+            ],
           ),
-        ],
+        ),
       ),
     );
   }
