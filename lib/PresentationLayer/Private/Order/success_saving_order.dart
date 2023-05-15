d9c4eec771fa3457517e2019d7f9052bbc232b12
AliAlsaeed176 <alialsaeed176@gmail.com>
2023-05-15 06:12:40 -0700

    Add navigation to new order after reset controllers and navigation to home screen

--- lib/PresentationLayer/Private/Order/success_saving_order.dart
+++ lib/PresentationLayer/Private/Order/success_saving_order.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:matjary/BussinessLayer/Controllers/order_controller.dart';
+import 'package:matjary/BussinessLayer/Controllers/order_screen_controller.dart';
+import 'package:matjary/Constants/get_routes.dart';
 import 'package:matjary/Constants/ui_colors.dart';
 import 'package:matjary/Constants/ui_styles.dart';
 import 'package:matjary/Constants/ui_text_styles.dart';
@@ -6,16 +10,19 @@ import 'package:matjary/PresentationLayer/Widgets/Public/accept_button.dart';
 import 'package:matjary/PresentationLayer/Widgets/Public/spacerHeight.dart';
 
 class SuccessSavingOrder extends StatelessWidget {
-  const SuccessSavingOrder({super.key});
+  SuccessSavingOrder({super.key});
+
+  final orderScreenController = Get.find<OrderScreenController>();
+  final orderController = Get.find<OrderController>();
 
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: EdgeInsets.symmetric(vertical: 60),
+      padding: const EdgeInsets.symmetric(vertical: 60),
       child: Column(
         children: [
           Expanded(
-            flex: 5,
+            flex: 4,
             child: Column(
               children: [
                 Column(
@@ -51,7 +58,11 @@ class SuccessSavingOrder extends StatelessWidget {
               children: [
                 AcceptButton(
                   text: 'إنشاء فاتورة جديدة',
-                  onPressed: () {},
+                  onPressed: () {
+                    orderController.resetOrder();
+                    orderScreenController.resetOrderScreen();
+                    orderScreenController.goToInitialPage();
+                  },
                 ),
                 spacerHeight(),
                 AcceptButton(
@@ -61,7 +72,9 @@ class SuccessSavingOrder extends StatelessWidget {
                     color: UIColors.smallText,
                   ),
                   backgroundColor: UIColors.white,
-                  onPressed: () {},
+                  onPressed: () {
+                    Get.back();
+                  },
                 ),
               ],
             ),
