93dd40f0f6935d6cb07719c19b8ff7131ece99cc
AliAlsaeed176 <alialsaeed176@gmail.com>
2023-05-22 03:36:22 -0700

    Get orders from orders controller

--- lib/PresentationLayer/Private/choose_order_screen.dart
+++ lib/PresentationLayer/Private/choose_order_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:matjary/BussinessLayer/Controllers/account_controller.dart';
 import 'package:matjary/BussinessLayer/Controllers/home_controller.dart';
+import 'package:matjary/BussinessLayer/Controllers/orders_controller.dart';
 import 'package:matjary/BussinessLayer/Controllers/ware_controller.dart';
 import 'package:matjary/Constants/get_routes.dart';
 import 'package:matjary/Constants/ui_colors.dart';
@@ -17,7 +18,7 @@ import 'package:matjary/PresentationLayer/Widgets/Public/spacerHeight.dart';
 class ChooseOrderScreen extends StatelessWidget {
   ChooseOrderScreen({super.key});
 
-  final HomeController homeController = Get.find<HomeController>();
+  final OrdersController ordersController = Get.find<OrdersController>();
 
   @override
   Widget build(BuildContext context) {
@@ -45,16 +46,16 @@ class ChooseOrderScreen extends StatelessWidget {
                 spacerHeight(height: 20),
                 Expanded(
                   child: GetBuilder(
-                    init: homeController,
-                    builder: (context) => homeController.orders.isEmpty
+                    init: ordersController,
+                    builder: (context) => ordersController.orders.isEmpty
                         ? Center(
                             child: loadingItem(width: 100, isWhite: true),
                           )
                         : ListView.separated(
                             itemBuilder: (context, index) {
                               return CustomBox(
-                                title:
-                                    homeController.orders[index].id.toString(),
+                                title: ordersController.orders[index].id
+                                    .toString(),
                                 editOnPressed: () {
                                   //Navigate to edit order screen
                                 },
@@ -69,7 +70,7 @@ class ChooseOrderScreen extends StatelessWidget {
                             separatorBuilder: (context, index) {
                               return spacerHeight();
                             },
-                            itemCount: homeController.orders.length,
+                            itemCount: ordersController.orders.length,
                           ),
                   ),
                 ),
