60aa7151ed740b6c1022f89ef82c9c1a8f52d055
AliAlsaeed176 <alialsaeed176@gmail.com>
2023-06-03 06:35:43 -0700

    Add new page

--- lib/Constants/get_pages.dart
+++ lib/Constants/get_pages.dart
@@ -3,6 +3,7 @@ import 'package:matjary/BussinessLayer/Bindings/home_binding.dart';
 import 'package:matjary/PresentationLayer/Auth/login_screen.dart';
 import 'package:matjary/PresentationLayer/Auth/pin_code_screen.dart';
 import 'package:matjary/PresentationLayer/Auth/register_screen.dart';
+import 'package:matjary/PresentationLayer/Private/orders/create_edit_order/order_details.dart';
 import 'package:matjary/PresentationLayer/Private/orders/create_edit_order/select_products_screen.dart';
 import 'package:matjary/PresentationLayer/Private/choose_account_screen.dart';
 import 'package:matjary/PresentationLayer/Private/choose_category_screen.dart';
@@ -17,6 +18,7 @@ import 'package:matjary/PresentationLayer/Private/orders/create_edit_order/creat
 import 'package:matjary/PresentationLayer/Private/create_edit_product_screen.dart';
 import 'package:matjary/PresentationLayer/Private/create_edit_ware.dart';
 import 'package:matjary/PresentationLayer/Private/create_statement.dart';
+import 'package:matjary/PresentationLayer/Private/orders/order_details_screen.dart';
 import 'package:matjary/PresentationLayer/Private/orders/order_screen.dart';
 import 'package:matjary/PresentationLayer/Private/orders/orders_screen.dart';
 import 'package:matjary/PresentationLayer/Public/home_screen.dart';
@@ -69,4 +71,5 @@ List<GetPage<dynamic>> getPages = [
   GetPage(name: AppRoutes.selectProducts, page: () => SelectProductsScreen()),
   GetPage(name: AppRoutes.ordersScreen, page: () => OrdersScreen()),
   GetPage(name: AppRoutes.orderScreen, page: () => OrderScreen()),
+  GetPage(name: AppRoutes.orderDetailsScreen, page: () => OrderDetailsScreen()),
 ];
