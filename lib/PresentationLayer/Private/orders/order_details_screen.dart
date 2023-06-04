2aade1a85ee5c824b253f17b713902254c61639b
AliAlsaeed176 <alialsaeed176@gmail.com>
2023-06-04 02:23:16 -0700

    Update screen to display order detials

--- lib/PresentationLayer/Private/orders/order_details_screen.dart
+++ lib/PresentationLayer/Private/orders/order_details_screen.dart
@@ -1,14 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:matjary/BussinessLayer/Controllers/order_controller.dart';
+import 'package:matjary/BussinessLayer/Controllers/products_controller.dart';
 import 'package:matjary/Constants/ui_colors.dart';
+import 'package:matjary/Constants/ui_text_styles.dart';
 import 'package:matjary/DataAccesslayer/Models/order.dart';
+import 'package:matjary/PresentationLayer/Widgets/Private/order_details_info_titles.dart';
+import 'package:matjary/PresentationLayer/Widgets/Private/order_details_item.dart';
 import 'package:matjary/PresentationLayer/Widgets/Public/custom_app_bar.dart';
 import 'package:matjary/PresentationLayer/Widgets/Public/custom_drawer.dart';
 import 'package:matjary/PresentationLayer/Widgets/Public/page_title.dart';
+import 'package:matjary/PresentationLayer/Widgets/Public/spacerHeight.dart';
 
 class OrderDetailsScreen extends StatelessWidget {
   OrderDetailsScreen({super.key});
 
+  final productsController = Get.find<ProductsController>();
+  final orderController = Get.find<OrderController>();
   Order order = Get.arguments;
 
   @override
@@ -25,6 +33,42 @@ class OrderDetailsScreen extends StatelessWidget {
             child: Column(
               children: [
                 PageTitle(title: 'فاتورة رقم  ${order.id}#'),
+                spacerHeight(height: 30),
+                const OrderDetailsInfoTitles(
+                  firstColumnTitle: 'المنتج',
+                  secondColumnTitle: 'الكمية',
+                  thirdColumnTitle: 'الافرادي',
+                  fourthColumnTitle: 'الإجمالي',
+                ),
+                spacerHeight(),
+                Expanded(
+                  flex: 7,
+                  child: ListView.separated(
+                    itemBuilder: (context, index) {
+                      return OrderDetailsItem(
+                        productName: productsController
+                            .getProductName(order.details[index]['product_id']),
+                        productQuantity: order.details[index]['quantity'],
+                        productPrice: order.details[index]['price'],
+                        productTotal: order.details[index]['total_price'],
+                      );
+                    },
+                    separatorBuilder: (context, index) =>
+                        spacerHeight(height: 10),
+                    itemCount: order.details.length,
+                  ),
+                ),
+                spacerHeight(),
+                OrderDetailsInfoTitles(
+                  titleTextStyle: UITextStyle.boldMeduim,
+                  firstColumnTitle: 'الإجمالي',
+                  secondColumnTitle: orderController
+                      .getOrderProductsQuantitiesCount(order.details)
+                      .toString(),
+                  thirdColumnTitle: '',
+                  fourthColumnTitle: order.total.toString(),
+                ),
+                spacerHeight(height: 60),
               ],
             ),
           ),
