b5166de824ba930f8e0b0e3cb23c8d106603f421
AliAlsaeed176 <alialsaeed176@gmail.com>
2023-06-04 06:47:41 -0700

    Add payment model

---
+++ lib/DataAccesslayer/Models/payment.dart
@@ -0,0 +1,39 @@
+class Payment {
+  String type;
+  int counterPartyId;
+  int bankId;
+  String statement;
+  num amount;
+  DateTime date;
+
+  Payment({
+    required this.type,
+    required this.counterPartyId,
+    required this.bankId,
+    required this.statement,
+    required this.amount,
+    required this.date,
+  });
+
+  Map<String, dynamic> toMap() {
+    return <String, dynamic>{
+      "type": type,
+      "account_id": counterPartyId,
+      "bank_id": bankId,
+      "amount": amount,
+      "statement": statement,
+      "created_at": date,
+    };
+  }
+
+  factory Payment.fromMap(Map<String, dynamic> map) {
+    return Payment(
+      type: map["type"],
+      counterPartyId: map["first_side_id"] as int,
+      bankId: map["second_side_id"] as int,
+      amount: map["amount"],
+      statement: map["statement"],
+      date: DateTime.parse(map['created_at']),
+    );
+  }
+}
