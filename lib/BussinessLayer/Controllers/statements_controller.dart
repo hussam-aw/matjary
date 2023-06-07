3840c24568e495ac204dcc13e20dddfed893be4c
AliAlsaeed176 <alialsaeed176@gmail.com>
2023-06-07 02:41:19 -0700

    Add get statements method

--- lib/BussinessLayer/Controllers/statements_controller.dart
+++ lib/BussinessLayer/Controllers/statements_controller.dart
@@ -1,3 +1,16 @@
 import 'package:get/get.dart';
+import 'package:matjary/DataAccesslayer/Models/statement.dart';
+import 'package:matjary/DataAccesslayer/Repositories/statement_repo.dart';
 
-class StatementController extends GetxController {}
+class StatementsController extends GetxController {
+  List<Statement> statements = [];
+  StatementRepo statementRepo = StatementRepo();
+  var isLoading = false.obs;
+
+  Future<void> getStatements() async {
+    isLoading.value = true;
+    statements = await statementRepo.getStatements();
+    isLoading.value = false;
+    print(statements);
+  }
+}
