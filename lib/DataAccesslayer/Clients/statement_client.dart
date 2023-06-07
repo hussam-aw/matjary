993825ed571c9b71b6913cff4fde17efa4f37f5f
AliAlsaeed176 <alialsaeed176@gmail.com>
2023-06-07 02:39:13 -0700

    Replace payment client with statemant client

--- lib/DataAccesslayer/Clients/statement_client.dart
+++ lib/DataAccesslayer/Clients/statement_client.dart
@@ -5,9 +5,9 @@ import 'package:matjary/Constants/api_links.dart';
 import 'package:matjary/main.dart';
 
 class StatementClinet {
-  Future<dynamic> getStatements(userId) async {
-    var response = await http.get(Uri.parse("$baseUrl$statementLink/useId"));
-
+  Future<dynamic> getStatements(id) async {
+    var response = await http.get(Uri.parse("$baseUrl$statementsLink/$id"));
+    print(response.body);
     if (response.statusCode == 200) {
       return response.body;
     } else {
@@ -22,6 +22,7 @@ class StatementClinet {
           "first_side_id": firstSideId,
           "second_side_id": secondSideId,
           "user_id": MyApp.appUser!.id,
+          'type': 'other',
           "statement": statement,
           "amount": amount,
           "date": date,
@@ -33,6 +34,30 @@ class StatementClinet {
     print(response.body);
     if (response.statusCode == 201) {
       return response.body;
+    } else {
+      return null;
+    }
+  }
+
+  Future<dynamic> createPayment(
+      type, accountId, bankId, statement, amount, date) async {
+    var response = await http.post(Uri.parse('$baseUrl$statementLink'),
+        body: jsonEncode(<String, dynamic>{
+          "type": type,
+          "account_id": accountId,
+          "bank_id": bankId,
+          "amount": amount,
+          "statement": statement,
+          "created_at": date,
+          "user_id": MyApp.appUser!.id,
+        }),
+        headers: <String, String>{
+          'Content-Type': 'application/json; charset=UTF-8',
+        });
+
+    print(response.body);
+    if (response.statusCode == 201) {
+      return true;
     } else {
       return null;
     }
