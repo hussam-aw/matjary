8718aefa2bfa073bf58daf44b24d2373de44b091
AliAlsaeed176 <alialsaeed176@gmail.com>
2023-06-11 06:21:17 -0700

    Create wares report screen and fetch all products report date

--- lib/PresentationLayer/Private/Reports/wares_report_screen.dart
+++ lib/PresentationLayer/Private/Reports/wares_report_screen.dart
@@ -1,10 +1,90 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:get/get.dart';
+import 'package:matjary/BussinessLayer/Controllers/reports_controller.dart';
+import 'package:matjary/Constants/ui_colors.dart';
+import 'package:matjary/Constants/ui_text_styles.dart';
+import 'package:matjary/PresentationLayer/Widgets/Private/table_details_item.dart';
+import 'package:matjary/PresentationLayer/Widgets/Private/table_titles.dart';
+import 'package:matjary/PresentationLayer/Widgets/Public/accept_icon_button.dart';
+import 'package:matjary/PresentationLayer/Widgets/Public/custom_app_bar.dart';
+import 'package:matjary/PresentationLayer/Widgets/Public/custom_drawer.dart';
+import 'package:matjary/PresentationLayer/Widgets/Public/loading_item.dart';
+import 'package:matjary/PresentationLayer/Widgets/Public/page_title.dart';
+import 'package:matjary/PresentationLayer/Widgets/Public/spacerHeight.dart';
 
 class WaresReportScreen extends StatelessWidget {
-  const WaresReportScreen({super.key});
+  WaresReportScreen({super.key});
+
+  final reportsController = Get.put(ReportsController());
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    reportsController.getAllProductsReports();
+    return Directionality(
+      textDirection: TextDirection.rtl,
+      child: Scaffold(
+        backgroundColor: UIColors.mainBackground,
+        appBar: customAppBar(showingAppIcon: false),
+        drawer: CustomDrawer(),
+        body: SafeArea(
+          child: Container(
+            padding: const EdgeInsets.symmetric(vertical: 15, horizontal: 30),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                PageTitle(title: 'جرد البضاعة الشامل'),
+                spacerHeight(height: 22),
+                TableTitles(
+                  isDecorated: true,
+                  firstColumnTitle: 'المنتج',
+                  secondColumnTitle: '',
+                  thirdColumnTitle: 'الكميات',
+                  fourthColumnTitle: 'التفاصيل',
+                ),
+                spacerHeight(),
+                Expanded(
+                  flex: 7,
+                  child: Obx(() {
+                    return reportsController.isloading.value
+                        ? Center(child: loadingItem(width: 100, isWhite: true))
+                        : ListView.separated(
+                            itemBuilder: (context, index) {
+                              return TableDetailsItem(
+                                firstColumnItem: reportsController
+                                    .productsReports[index].productName,
+                                thirdColumnItem: reportsController
+                                    .productsReports[index].quantity
+                                    .toString(),
+                                fourthColumnItem: InkWell(
+                                  onTap: () {},
+                                  child: Icon(
+                                    Icons.info,
+                                    size: 30,
+                                    color: UIColors.white,
+                                  ),
+                                ),
+                              );
+                            },
+                            separatorBuilder: (context, index) =>
+                                spacerHeight(height: 10),
+                            itemCount: reportsController.productsReports.length,
+                          );
+                  }),
+                ),
+                spacerHeight(height: 22),
+                AccetpIconButton(
+                  text: const Text('حفظ pdf', style: UITextStyle.boldMeduim),
+                  icon: const Icon(FontAwesomeIcons.solidFloppyDisk),
+                  center: true,
+                  backgroundColor: UIColors.primary,
+                  onPressed: () {},
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
