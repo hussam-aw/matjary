565bf700e7321249b66729506f862d90034b22b7
AliAlsaeed176 <alialsaeed176@gmail.com>
2023-06-11 06:20:00 -0700

    Add get all products method

--- lib/BussinessLayer/Controllers/reports_controller.dart
+++ lib/BussinessLayer/Controllers/reports_controller.dart
@@ -5,7 +5,8 @@ import 'package:matjary/DataAccesslayer/Repositories/reports_repo.dart';
 
 class ReportsController extends GetxController {
   List<WareReport> wareReports = [];
-  List<ProductReport> productReports = [];
+  ProductReport? productReport = null;
+  List<ProductReport> productsReports = [];
   ReportsRepo reportsRepo = ReportsRepo();
   var isloading = false.obs;
 
@@ -15,9 +16,15 @@ class ReportsController extends GetxController {
     isloading.value = false;
   }
 
-  Future<void> getProductReports(productId) async {
+  Future<void> getProductReport(productId) async {
     isloading.value = true;
-    productReports = await reportsRepo.getProductReports(productId);
+    productReport = await reportsRepo.getProductReport(productId);
+    isloading.value = false;
+  }
+
+  Future<void> getAllProductsReports() async {
+    isloading.value = true;
+    productsReports = await reportsRepo.getAllProductsReports();
     isloading.value = false;
   }
 }
