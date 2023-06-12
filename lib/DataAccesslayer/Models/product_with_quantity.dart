e582364c320ad626f9071c5c697e1a089e7b3cc3
AliAlsaeed176 <alialsaeed176@gmail.com>
2023-06-12 03:29:40 -0700

    Create product with quantity model

---
+++ lib/DataAccesslayer/Models/product_with_quantity.dart
@@ -0,0 +1,25 @@
+import 'product.dart';
+
+class ProductWithQuantity {
+  int productId;
+  int quantity;
+
+  ProductWithQuantity({
+    required this.productId,
+    required this.quantity,
+  });
+
+  Map<String, dynamic> toMap() {
+    return <String, dynamic>{
+      'id': productId,
+      'quantity': quantity,
+    };
+  }
+
+  factory ProductWithQuantity.fromMap(Map<String, dynamic> map) {
+    return ProductWithQuantity(
+      productId: map['id'] as int,
+      quantity: map['quantity'],
+    );
+  }
+}
