b52756211ef2203b32f1c6fc333a1e4addfce9b5
AliAlsaeed176 <alialsaeed176@gmail.com>
2023-06-18 05:42:00 -0700

    Use get cash amount method

--- lib/BussinessLayer/Controllers/home_controller.dart
+++ lib/BussinessLayer/Controllers/home_controller.dart
@@ -112,6 +112,7 @@ class HomeController extends GetxController {
   void fetchData() async {
     //isLoading.value = true;
     ordersController.getOrders();
+    accountsController.getCachAmount();
     await accountsController.getAccounts();
     accountsController.getClientAcoounts();
     accountsController.getBankAcoounts();
