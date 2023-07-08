429d328866a9b08523dec06fa99f07da0a94d978
AliAlsaeed176 <alialsaeed176@gmail.com>
2023-07-08 00:54:20 -0700

    Remove unnecessary import

--- lib/PresentationLayer/Auth/login_screen.dart
+++ lib/PresentationLayer/Auth/login_screen.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:matjary/Constants/get_routes.dart';
 import 'package:matjary/Constants/ui_colors.dart';
-import 'package:matjary/Constants/ui_styles.dart';
 import 'package:matjary/PresentationLayer/Widgets/Public/accept_button.dart';
 import 'package:matjary/PresentationLayer/Widgets/Public/app_icon_header.dart';
 import 'package:matjary/PresentationLayer/Widgets/Public/custom_text_form_field.dart';
