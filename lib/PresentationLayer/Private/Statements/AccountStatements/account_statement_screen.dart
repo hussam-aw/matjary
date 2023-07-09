6ddc77c8caee0c35a0e5016e1f53f508a0767ae3
AliAlsaeed176 <alialsaeed176@gmail.com>
2023-07-09 06:38:55 -0700

    Remove unnecessary import

--- lib/PresentationLayer/Private/Statements/AccountStatements/account_statement_screen.dart
+++ lib/PresentationLayer/Private/Statements/AccountStatements/account_statement_screen.dart
@@ -8,7 +8,6 @@ import 'package:matjary/Constants/ui_text_styles.dart';
 import 'package:matjary/DataAccesslayer/Models/account.dart';
 import 'package:matjary/PresentationLayer/Widgets/Private/Statements/account_movement_box.dart';
 import 'package:matjary/PresentationLayer/Widgets/Private/Statements/account_statement_header.dart';
-import 'package:matjary/PresentationLayer/Widgets/Public/accept_button.dart';
 import 'package:matjary/PresentationLayer/Widgets/Public/accept_icon_button.dart';
 import 'package:matjary/PresentationLayer/Widgets/Public/custom_app_bar.dart';
 import 'package:matjary/PresentationLayer/Widgets/Public/custom_drawer.dart';
