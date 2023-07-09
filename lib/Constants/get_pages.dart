d01ad4693acdbbe8bb59b13ba09f4f8c86620762
AliAlsaeed176 <alialsaeed176@gmail.com>
2023-07-09 00:31:51 -0700

    Move create statement and payment files to new folder

--- lib/Constants/get_pages.dart
+++ lib/Constants/get_pages.dart
@@ -7,7 +7,7 @@ import 'package:matjary/PresentationLayer/Private/Reports/create_ware_report_scr
 import 'package:matjary/PresentationLayer/Private/Reports/product_qty_report_screen.dart';
 import 'package:matjary/PresentationLayer/Private/Reports/single_ware_report_screen.dart';
 import 'package:matjary/PresentationLayer/Private/Reports/wares_report_screen.dart';
-import 'package:matjary/PresentationLayer/Private/create_edit_payment_screen.dart';
+import 'package:matjary/PresentationLayer/Private/Statements/Payments/create_edit_payment_screen.dart';
 import 'package:matjary/PresentationLayer/Private/orders/create_edit_order/select_products_screen.dart';
 import 'package:matjary/PresentationLayer/Private/choose_account_screen.dart';
 import 'package:matjary/PresentationLayer/Private/choose_category_screen.dart';
@@ -15,17 +15,15 @@ import 'package:matjary/PresentationLayer/Private/choose_order_screen.dart';
 import 'package:matjary/PresentationLayer/Private/choose_product_screen.dart';
 import 'package:matjary/PresentationLayer/Private/choose_ware_screen.dart';
 import 'package:matjary/PresentationLayer/Private/create_edit_account_screen.dart';
-import 'package:matjary/PresentationLayer/Private/create_edit_bank_screen.dart';
 import 'package:matjary/PresentationLayer/Private/create_edit_category_screen.dart';
-import 'package:matjary/PresentationLayer/Private/create_edit_client_screen.dart';
 import 'package:matjary/PresentationLayer/Private/orders/create_edit_order/create_edit_order_screen.dart';
 import 'package:matjary/PresentationLayer/Private/create_edit_product_screen.dart';
 import 'package:matjary/PresentationLayer/Private/create_edit_ware.dart';
-import 'package:matjary/PresentationLayer/Private/create_statement.dart';
+import 'package:matjary/PresentationLayer/Private/Statements/create_statement.dart';
 import 'package:matjary/PresentationLayer/Private/orders/order_details_screen.dart';
 import 'package:matjary/PresentationLayer/Private/orders/order_screen.dart';
 import 'package:matjary/PresentationLayer/Private/orders/orders_screen.dart';
-import 'package:matjary/PresentationLayer/Private/payments_screen.dart';
+import 'package:matjary/PresentationLayer/Private/Statements/Payments/payments_screen.dart';
 import 'package:matjary/PresentationLayer/Private/Reports/product_report_screen.dart';
 import 'package:matjary/PresentationLayer/Private/profile_screen.dart';
 import 'package:matjary/PresentationLayer/Public/home_screen.dart';
@@ -49,12 +47,6 @@ List<GetPage<dynamic>> getPages = [
       page: () => CreateEditAccountScreen()),
   GetPage(
       name: AppRoutes.createEditWareScreen, page: () => CreateEditWareScreen()),
-  GetPage(
-      name: AppRoutes.createEditBankScreen,
-      page: () => const CreateEditBankScreen()),
-  GetPage(
-      name: AppRoutes.createEditClientScreen,
-      page: () => const CreateEditClientScreen()),
   GetPage(
       name: AppRoutes.createEditProductScreen,
       page: () => CreateEditProductScreen()),
