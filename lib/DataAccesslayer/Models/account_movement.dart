85088fc1d69252c53c76e06df9fec2cbd25b495c
AliAlsaeed176 <alialsaeed176@gmail.com>
2023-07-10 05:58:57 -0700

    Create account movememnt model

---
+++ lib/DataAccesslayer/Models/account_movement.dart
@@ -0,0 +1,31 @@
+class AccountMovement {
+  String type;
+  String statement;
+  num amount;
+  DateTime date;
+
+  AccountMovement({
+    required this.type,
+    required this.statement,
+    required this.amount,
+    required this.date,
+  });
+
+  Map<String, dynamic> toMap() {
+    return <String, dynamic>{
+      'type': type,
+      'statement': statement,
+      'amount': amount,
+      'date': date,
+    };
+  }
+
+  factory AccountMovement.fromMap(Map<String, dynamic> map) {
+    return AccountMovement(
+      type: '',
+      statement: map['statement'] ?? '',
+      amount: map['amount'],
+      date: DateTime.parse(map['date']),
+    );
+  }
+}
