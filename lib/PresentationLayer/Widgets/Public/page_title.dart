fa3307916eefb7ff8859a2c42ebc6e4bbf6ef615
AliAlsaeed176 <alialsaeed176@gmail.com>
2023-07-12 06:42:37 -0700

    Add title color property

--- lib/PresentationLayer/Widgets/Public/page_title.dart
+++ lib/PresentationLayer/Widgets/Public/page_title.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:matjary/Constants/ui_colors.dart';
 import 'package:matjary/Constants/ui_text_styles.dart';
 
 class PageTitle extends StatelessWidget {
-  const PageTitle({super.key, required this.title});
+  const PageTitle({
+    super.key,
+    required this.title,
+    this.titleColor = UIColors.white,
+  });
 
   final String title;
+  final Color titleColor;
 
   @override
   Widget build(BuildContext context) {
@@ -16,7 +22,9 @@ class PageTitle extends StatelessWidget {
           title,
           softWrap: true,
           //overflow: TextOverflow.ellipsis,
-          style: UITextStyle.boldHeading,
+          style: UITextStyle.boldHeading.copyWith(
+            color: titleColor,
+          ),
         ),
       ),
     );
