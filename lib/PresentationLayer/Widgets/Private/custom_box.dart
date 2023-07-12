cb15edb7d7b8c7ec5161f49cd396f6a81b5d3917
AliAlsaeed176 <alialsaeed176@gmail.com>
2023-07-12 06:47:10 -0700

    Refactor widget to accept more properities and fix its design

--- lib/PresentationLayer/Widgets/Private/custom_box.dart
+++ lib/PresentationLayer/Widgets/Private/custom_box.dart
@@ -1,74 +1,74 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:matjary/Constants/ui_colors.dart';
+import 'package:matjary/Constants/ui_styles.dart';
 import 'package:matjary/Constants/ui_text_styles.dart';
+import 'package:matjary/PresentationLayer/Widgets/Public/spacerHeight.dart';
+import 'package:matjary/PresentationLayer/Widgets/Public/spacerWidth.dart';
 
 class CustomBox extends StatelessWidget {
   const CustomBox({
     super.key,
     required this.title,
+    this.subTitle,
+    this.leading,
     required this.onTap,
   });
 
   final String title;
+  final String? subTitle;
+  final Widget? leading;
   final Function() onTap;
-  // final Function() editOnPressed;
-  // final String deleteDialogTitle;
-  // final String deleteDialogButtonText;
-  // final Function() deleteOnPressed;
 
   @override
   Widget build(BuildContext context) {
     return InkWell(
       onTap: onTap,
       child: Container(
-        //height: 67,
-        padding: const EdgeInsets.symmetric(vertical: 18, horizontal: 10),
+        width: Get.width,
+        padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 25),
         decoration: const BoxDecoration(
           color: UIColors.containerBackground,
+          borderRadius: raduis15,
         ),
-        child: Text(
-          title,
-          softWrap: true,
-          //overflow: TextOverflow.ellipsis,
-          textAlign: TextAlign.center,
-          style: UITextStyle.normalMeduim.copyWith(
-            color: UIColors.lightNormalText,
-          ),
+        child: Row(
+          children: [
+            if (leading != null)
+              Row(
+                children: [
+                  leading!,
+                  spacerWidth(),
+                ],
+              ),
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    title,
+                    softWrap: true,
+                    //overflow: TextOverflow.ellipsis,
+                    style: UITextStyle.normalMeduim.copyWith(
+                      color: UIColors.lightNormalText,
+                    ),
+                  ),
+                  spacerHeight(),
+                  if (subTitle != null)
+                    Text(
+                      subTitle!,
+                      softWrap: true,
+                      //overflow: TextOverflow.ellipsis,
+                      style: UITextStyle.normalBody.copyWith(
+                        color: UIColors.lightNormalText,
+                        height: 1.3,
+                      ),
+                    ),
+                ],
+              ),
+            ),
+          ],
         ),
       ),
     );
   }
 }
-
-// SizedBox(
-//             width: 80,
-//             //height: 50,
-//             child: Row(
-//               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-//               children: [
-//                 InkWell(
-//                   onTap: editOnPressed,
-//                   child: const Icon(
-//                     FontAwesomeIcons.penToSquare,
-//                     color: UIColors.white,
-//                   ),
-//                 ),
-//                 InkWell(
-//                   onTap: () {
-//                     Get.dialog(
-//                       CustomDialog(
-//                         title: deleteDialogTitle,
-//                         buttonText: deleteDialogButtonText,
-//                         confirmOnPressed: deleteOnPressed,
-//                       ),
-//                     );
-//                   },
-//                   child: const Icon(
-//                     Icons.delete,
-//                     color: UIColors.white,
-//                     size: 27,
-//                   ),
-//                 ),
-//               ],
-//             ),
-//           ),
