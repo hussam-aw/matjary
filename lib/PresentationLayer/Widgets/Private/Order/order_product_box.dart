8aa58e176cc1fb61f56037723dc3fb397aa327d6
AliAlsaeed176 <alialsaeed176@gmail.com>
2023-07-13 06:53:25 -0700

    Refactor and update widget code to make it more dynamic

--- lib/PresentationLayer/Widgets/Private/Order/order_product_box.dart
+++ lib/PresentationLayer/Widgets/Private/Order/order_product_box.dart
@@ -1,157 +1,163 @@
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
-import 'package:matjary/BussinessLayer/Controllers/order_screen_controller.dart';
 import 'package:matjary/Constants/ui_colors.dart';
 import 'package:matjary/Constants/ui_styles.dart';
 import 'package:matjary/Constants/ui_text_styles.dart';
-import 'package:matjary/DataAccesslayer/Models/product.dart';
 import 'package:matjary/PresentationLayer/Widgets/Public/spacerHeight.dart';
 import 'package:matjary/PresentationLayer/Widgets/Public/spacerWidth.dart';
 
 class OrderProductBox extends StatelessWidget {
   OrderProductBox({
     super.key,
-    required this.product,
+    required this.productName,
     required this.quantity,
     required this.price,
     required this.totalPrice,
+    this.backgroundColor,
+    this.screenMode,
   });
 
-  final Product product;
+  final String productName;
   final int? quantity;
   final num? price;
   final num totalPrice;
-  final orderScreenController = Get.find<OrderScreenController>();
+  final Color? backgroundColor;
+  final String? screenMode;
 
   @override
   Widget build(BuildContext context) {
-    return InkWell(
-      onLongPress: () {
-        orderScreenController.openUpdateProductBottomSheet(product);
-      },
-      child: GetBuilder(
-          init: orderScreenController,
-          builder: (context) {
-            return Container(
-              //height: 90,
-              padding: const EdgeInsets.symmetric(horizontal: 25, vertical: 18),
-              decoration: BoxDecoration(
-                color: orderScreenController
-                        .checkIfProductSelectedForUpdate(product)
-                    ? UIColors.primary
-                    : UIColors.containerBackground,
-                borderRadius: raduis15,
-              ),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Expanded(
-                    flex: 4,
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          product.name,
-                          softWrap: true,
-                          //overflow: TextOverflow.ellipsis,
-                          style: UITextStyle.normalBody,
-                        ),
-                        spacerHeight(),
-                        Row(
-                          children: [
-                            const Text(
-                              'الكمية:',
-                              softWrap: true,
-                              style: UITextStyle.small,
-                            ),
-                            spacerWidth(),
-                            Text(
-                              quantity.toString(),
-                              softWrap: true,
-                              style: UITextStyle.normalSmall,
-                            ),
-                            spacerWidth(width: 10),
-                            const Text(
-                              'الافرادي:',
-                              softWrap: true,
-                              style: UITextStyle.small,
-                            ),
-                            spacerWidth(),
-                            Text(
-                              price!.toStringAsFixed(2),
-                              softWrap: true,
-                              style: UITextStyle.normalSmall,
-                            ),
-                          ],
-                        ),
-                      ],
+    Color textColor = screenMode == null ? UIColors.white : UIColors.printText;
+    return Container(
+      //height: 90,
+      padding: const EdgeInsets.symmetric(horizontal: 25, vertical: 18),
+      decoration: BoxDecoration(
+        color: screenMode == null ? backgroundColor : UIColors.white,
+        borderRadius: raduis15,
+        border: screenMode != null
+            ? Border.all(color: UIColors.containerBorder)
+            : null,
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Expanded(
+            flex: 4,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  productName,
+                  softWrap: true,
+                  //overflow: TextOverflow.ellipsis,
+                  style: UITextStyle.normalBody.copyWith(
+                    color: textColor,
+                  ),
+                ),
+                spacerHeight(),
+                Row(
+                  children: [
+                    Text(
+                      'الكمية:',
+                      softWrap: true,
+                      style: UITextStyle.small.copyWith(
+                        color: textColor,
+                      ),
+                    ),
+                    spacerWidth(),
+                    Text(
+                      quantity.toString(),
+                      softWrap: true,
+                      style: UITextStyle.normalSmall.copyWith(
+                        color: textColor,
+                      ),
+                    ),
+                    spacerWidth(width: 10),
+                    Text(
+                      'الافرادي:',
+                      softWrap: true,
+                      style: UITextStyle.small.copyWith(
+                        color: textColor,
+                      ),
+                    ),
+                    spacerWidth(),
+                    Text(
+                      price!.toStringAsFixed(2),
+                      softWrap: true,
+                      style: UITextStyle.normalSmall.copyWith(
+                        color: textColor,
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+          spacerWidth(),
+          Expanded(
+            flex: 1,
+            child: Column(
+              children: [
+                Text(
+                  'الإجمالي',
+                  softWrap: true,
+                  //overflow: TextOverflow.ellipsis,
+                  style: UITextStyle.normalBody.copyWith(
+                    color: textColor,
+                  ),
+                ),
+                spacerHeight(),
+                Align(
+                  alignment: Alignment.center,
+                  child: Text(
+                    totalPrice.toStringAsFixed(1),
+                    softWrap: true,
+                    //overflow: TextOverflow.ellipsis,
+                    style: UITextStyle.normalMeduim.copyWith(
+                      color: textColor,
                     ),
                   ),
-                  spacerWidth(),
-                  Expanded(
-                    flex: 1,
-                    child: Column(
-                      children: [
-                        const Text(
-                          'الإجمالي',
-                          softWrap: true,
-                          //overflow: TextOverflow.ellipsis,
-                          style: UITextStyle.normalBody,
-                        ),
-                        spacerHeight(),
-                        Align(
-                          alignment: Alignment.center,
-                          child: Text(
-                            totalPrice.toStringAsFixed(1),
-                            softWrap: true,
-                            //overflow: TextOverflow.ellipsis,
-                            style: UITextStyle.normalMeduim,
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                  // Expanded(
-                  //   flex: 1,
-                  //   child: Column(
-                  //     mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  //     crossAxisAlignment: CrossAxisAlignment.end,
-                  //     children: [
-                  //       InkWell(
-                  //         onTap: () {
-                  //           Get.bottomSheet(UpdateOrderProductBottomSheet(
-                  //             productId: product.id,
-                  //             currentQuantity: orderScreenController
-                  //                 .selectedProductsQuantities[product.id],
-                  //             currentPrice: orderScreenController
-                  //                 .selectedProductPrices[product.id],
-                  //           ));
-                  //         },
-                  //         child: const Icon(
-                  //           FontAwesomeIcons.penToSquare,
-                  //           size: 17,
-                  //           color: UIColors.white,
-                  //         ),
-                  //       ),
-                  //       spacerHeight(),
-                  //       InkWell(
-                  //         onTap: () {
-                  //           orderScreenController.deleteSelectedProduct(product.id);
-                  //         },
-                  //         child: const Icon(
-                  //           Icons.delete,
-                  //           size: 20,
-                  //           color: UIColors.white,
-                  //         ),
-                  //       ),
-                  //     ],
-                  //   ),
-                  // ),
-                ],
-              ),
-            );
-          }),
+                ),
+              ],
+            ),
+          ),
+          // Expanded(
+          //   flex: 1,
+          //   child: Column(
+          //     mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          //     crossAxisAlignment: CrossAxisAlignment.end,
+          //     children: [
+          //       InkWell(
+          //         onTap: () {
+          //           Get.bottomSheet(UpdateOrderProductBottomSheet(
+          //             productId: product.id,
+          //             currentQuantity: orderScreenController
+          //                 .selectedProductsQuantities[product.id],
+          //             currentPrice: orderScreenController
+          //                 .selectedProductPrices[product.id],
+          //           ));
+          //         },
+          //         child: const Icon(
+          //           FontAwesomeIcons.penToSquare,
+          //           size: 17,
+          //           color: UIColors.white,
+          //         ),
+          //       ),
+          //       spacerHeight(),
+          //       InkWell(
+          //         onTap: () {
+          //           orderScreenController.deleteSelectedProduct(product.id);
+          //         },
+          //         child: const Icon(
+          //           Icons.delete,
+          //           size: 20,
+          //           color: UIColors.white,
+          //         ),
+          //       ),
+          //     ],
+          //   ),
+          // ),
+        ],
+      ),
     );
   }
 }
