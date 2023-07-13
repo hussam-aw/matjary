81ad106b0dda8ef83fc3b9f7aa9535b15c560616
AliAlsaeed176 <alialsaeed176@gmail.com>
2023-07-13 06:50:54 -0700

    Add new route and fix some others

--- lib/Constants/get_routes.dart
+++ lib/Constants/get_routes.dart
@@ -21,21 +21,20 @@ class AppRoutes {
   static const chooseCategoryScreen = '/home/chooseCategoryScreen';
   static const createEditOrderScreen = '/home/createEditOrderScreen';
   static const ordersScreen = '/home/ordersScreen';
-  static const selectProducts = '/home/createEditOrderScreen/selectProducts';
-  static const orderScreen = '/home/ordersScreen/orderScreen';
-  static const orderDetailsScreen =
-      '/home/ordersScreen/orderScreen/orderDetailsScreen';
+  static const selectProducts = '/createEditOrderScreen/selectProducts';
+  static const orderScreen = '/ordersScreen/orderScreen';
+  static const orderPrintScreen = '/orderScreen/orderPrintScreen';
+  static const orderDetailsScreen = '/orderScreen/orderDetailsScreen';
   static const createEditPaymentScreen = '/home/createEditPaymentScreen';
   static const paymentsScreen = '/home/paymentsScreen';
   static const productReportScreen = '/home/productReportScreen';
   static const createWareReportScreen =
-      '/home/productReportScreen/createWareReportScreen';
+      '/productReportScreen/createWareReportScreen';
   static const singleWareReportScreen =
-      '/home/productReportScreen/singleWareReportScreen';
-  static const waresReportScreen =
-      '/home/productReportScreen/waresReportScreen';
+      '/productReportScreen/singleWareReportScreen';
+  static const waresReportScreen = '/productReportScreen/waresReportScreen';
   static const productQtyReportScreen =
-      '/home/productReportScreen/chooseProductScreen/productQtyReportScreen';
+      '/chooseProductScreen/productQtyReportScreen';
   static const profileScreen = '/home/profileScreen';
   static const accountStatementTypeScreen = '/home/accountStatementTypeScreen';
   static const accountStatementScreen =
