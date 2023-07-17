c9f7289b3114fc6967aa97ff810024315801e547
AliAlsaeed176 <alialsaeed176@gmail.com>
2023-07-17 04:12:24 -0700

    Update used app user and app user in storage

--- lib/BussinessLayer/Controllers/profile_controller.dart
+++ lib/BussinessLayer/Controllers/profile_controller.dart
@@ -1,13 +1,16 @@
 import 'package:flutter/widgets.dart';
 import 'package:get/get.dart';
+import 'package:matjary/DataAccesslayer/Clients/box_client.dart';
 import 'package:matjary/DataAccesslayer/Repositories/user_repo.dart';
 import 'package:matjary/PresentationLayer/Widgets/snackbars.dart';
+import 'package:matjary/main.dart';
 
 class ProfileController extends GetxController {
   TextEditingController userNameController = TextEditingController();
   TextEditingController mobileNumberController = TextEditingController();
   TextEditingController passwordController = TextEditingController();
   UserRepo userRepo = UserRepo();
+  BoxClient client = BoxClient();
   var isLoading = false.obs;
 
   Future<void> updateProfile() async {
@@ -20,6 +23,9 @@ class ProfileController extends GetxController {
           await userRepo.updateUserInfo(userName, mobilePhone, password, '');
       isLoading.value = false;
       if (user != null) {
+        MyApp.appUser = user;
+        await client.setAuthedUser(user);
+        update();
         SnackBars.showSuccess('تم تعديل بيانات الملف الشخصي');
       } else {
         SnackBars.showError('فشل التعديل');
