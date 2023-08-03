1b6f7cabd7bddfcde010749b04e1f3b398ff3fb6
AliAlsaeed176 <alialsaeed176@gmail.com>
2023-08-03 02:54:46 -0700

    Add onTap property

--- lib/PresentationLayer/Widgets/Private/Statements/statement_type_box.dart
+++ lib/PresentationLayer/Widgets/Private/Statements/statement_type_box.dart
@@ -5,42 +5,48 @@ import 'package:matjary/Constants/ui_text_styles.dart';
 import 'package:matjary/PresentationLayer/Widgets/Public/spacerHeight.dart';
 
 class StatementTypeBox extends StatelessWidget {
-  const StatementTypeBox(
-      {super.key,
-      required this.statementTypeIcon,
-      required this.statementType});
+  const StatementTypeBox({
+    super.key,
+    required this.statementTypeIcon,
+    required this.statementType,
+    this.onTap,
+  });
 
   final IconData statementTypeIcon;
   final String statementType;
+  final Function()? onTap;
 
   @override
   Widget build(BuildContext context) {
     return Expanded(
       flex: 2,
-      child: Container(
-        //height: 70,
-        decoration: const BoxDecoration(
-          color: UIColors.primary,
-          borderRadius: raduis10,
-        ),
-        child: Center(
-          child: Padding(
-            padding: const EdgeInsets.symmetric(vertical: 5),
-            child: Column(
-              children: [
-                Icon(
-                  statementTypeIcon,
-                  size: 35,
-                  color: UIColors.white,
-                ),
-                spacerHeight(),
-                Text(
-                  statementType,
-                  softWrap: true,
-                  //overflow: TextOverflow.ellipsis,
-                  style: UITextStyle.normalMeduim,
-                ),
-              ],
+      child: InkWell(
+        onTap: onTap,
+        child: Container(
+          //height: 70,
+          decoration: const BoxDecoration(
+            color: UIColors.primary,
+            borderRadius: raduis10,
+          ),
+          child: Center(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(vertical: 5),
+              child: Column(
+                children: [
+                  Icon(
+                    statementTypeIcon,
+                    size: 35,
+                    color: UIColors.white,
+                  ),
+                  spacerHeight(),
+                  Text(
+                    statementType,
+                    softWrap: true,
+                    //overflow: TextOverflow.ellipsis,
+                    style: UITextStyle.normalMeduim,
+                  ),
+                ],
+              ),
             ),
           ),
         ),
