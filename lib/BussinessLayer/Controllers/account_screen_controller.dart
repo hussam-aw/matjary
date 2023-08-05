4d179fd14e963e5f1df2c5ec698b31c5748d7813
AliAlsaeed176 <alialsaeed176@gmail.com>
2023-08-05 07:40:38 -0700

    Add new methods to set account style for information, select account image and set account details

--- lib/BussinessLayer/Controllers/account_screen_controller.dart
+++ lib/BussinessLayer/Controllers/account_screen_controller.dart
@@ -1,6 +1,12 @@
 import 'package:get/get.dart';
+import 'package:matjary/BussinessLayer/helpers/image_picker_helper.dart';
+import 'package:matjary/DataAccesslayer/Models/account.dart';
 
 class AccountScreenController extends GetxController {
+  bool accountStyleForInformation = false;
+  ImagePickerHelper imagePickerHelper = ImagePickerHelper();
+  RxString selectedAccountImage = ''.obs;
+
   List<String> accountTypes = [
     'مدين',
     'دائن',
@@ -9,6 +15,15 @@ class AccountScreenController extends GetxController {
   Map<int, String> counterAccountTypes = {
     0: 'مدين',
     1: 'دائن',
+  };
+
+  Map<int, String> counterAccountStyles = {
+    1: 'صندوق',
+    2: 'زبون',
+    3: 'مورد',
+    4: 'جهة عمل',
+    6: 'حساب عادي',
+    10: 'مسوق',
   };
 
   RxMap<String, bool> accountTypesSelection = {
@@ -27,6 +42,40 @@ class AccountScreenController extends GetxController {
     if (type != null) {
       resetAccountTypes();
       accountTypesSelection[type] = true;
+    }
+  }
+
+  bool checkAccountStyleForInformation(style) {
+    switch (style) {
+      case 'حساب عادي':
+        return false;
+      case 'صندوق':
+        return false;
+      default:
+        return true;
+    }
+  }
+
+  void setAccountStyleForInformation(style) {
+    accountStyleForInformation = checkAccountStyleForInformation(style);
+  }
+
+  void changeAccountStyleForInformation(style) {
+    setAccountStyleForInformation(style);
+    update();
+  }
+
+  void selectAccountImage() async {
+    selectedAccountImage.value = await imagePickerHelper.pickImage();
+  }
+
+  void setAcountDetails(Account? account) {
+    if (account != null) {
+      setAccountType(counterAccountTypes[account.type]);
+      setAccountStyleForInformation(counterAccountStyles[account.style]);
+    } else {
+      setAccountType('مدين');
+      setAccountStyleForInformation('حساب عادي');
     }
   }
 }
