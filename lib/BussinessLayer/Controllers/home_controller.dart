d6b220a2ca35be082e1c8a3d77fecd9f81f5ad4d
AliAlsaeed176 <alialsaeed176@gmail.com>
2023-10-03 06:18:17 -0700

    Check connectivity state in fetch data

--- lib/BussinessLayer/Controllers/home_controller.dart
+++ lib/BussinessLayer/Controllers/home_controller.dart
@@ -1,6 +1,7 @@
 import 'package:get/get.dart';
 import 'package:matjary/BussinessLayer/Controllers/accounts_controller.dart';
 import 'package:matjary/BussinessLayer/Controllers/categories_controller.dart';
+import 'package:matjary/BussinessLayer/Controllers/connectivity_controller.dart';
 import 'package:matjary/BussinessLayer/Controllers/earns_expenses_controller.dart';
 import 'package:matjary/BussinessLayer/Controllers/orders_controller.dart';
 import 'package:matjary/BussinessLayer/Controllers/payments_controller.dart';
@@ -10,7 +11,6 @@ import 'package:matjary/BussinessLayer/Controllers/users_controller.dart';
 import 'package:matjary/BussinessLayer/Controllers/wares_controller.dart';
 
 class HomeController extends GetxController {
-
   AccountsController accountsController = Get.put(AccountsController());
   CategoriesController categoriesController = Get.put(CategoriesController());
   OrdersController ordersController = Get.put(OrdersController());
@@ -22,23 +22,26 @@ class HomeController extends GetxController {
   UsersController usersController = Get.put(UsersController());
   StoreSettingsController storeSettingsController =
       Get.put(StoreSettingsController());
+  final connectivityController = Get.find<ConnectivityController>();
 
   void fetchData() async {
-    //isLoading.value = true;
-    await accountsController.getCachAmount();
-    await ordersController.getOrders();
-    await accountsController.getAccounts();
-    ordersController.getPurchasesOrders();
-    ordersController.getSalesOrders();
-    //await accountsController.getPinnedAccounts();
-    productsController.getProducts();
-    categoriesController.getCategories();
-    paymentsController.getPayments();
-    waresController.getWares();
-    earnsExpensesController.getStatements();
-    usersController.getUsers();
-    storeSettingsController.getStoreSettings();
-    //isLoading.value = false;
+    if (connectivityController.isConnected) {
+      //isLoading.value = true;
+      await accountsController.getCachAmount();
+      await ordersController.getOrders();
+      await accountsController.getAccounts();
+      ordersController.getPurchasesOrders();
+      ordersController.getSalesOrders();
+      //await accountsController.getPinnedAccounts();
+      productsController.getProducts();
+      categoriesController.getCategories();
+      //paymentsController.getPayments();
+      waresController.getWares();
+      earnsExpensesController.getStatements();
+      usersController.getUsers();
+      storeSettingsController.getStoreSettings();
+      //isLoading.value = false;
+    }
   }
 
   @override
