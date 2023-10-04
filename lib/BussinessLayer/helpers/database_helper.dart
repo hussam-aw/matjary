3bd6dafe8839609b04eb6221475bf894c669a698
AliAlsaeed176 <alialsaeed176@gmail.com>
2023-10-04 07:38:15 -0700

    Upte init database method and add insert, get, delete methods

--- lib/BussinessLayer/helpers/database_helper.dart
+++ lib/BussinessLayer/helpers/database_helper.dart
@@ -1,9 +1,10 @@
+import 'package:matjary/Constants/app_strings.dart';
 import 'package:path/path.dart';
 import 'package:sqflite/sqflite.dart';
 
-class MatjaryDatabaseHelper {
-  MatjaryDatabaseHelper._();
-  static final MatjaryDatabaseHelper db = MatjaryDatabaseHelper._();
+class DatabaseHelper {
+  DatabaseHelper._();
+  static final DatabaseHelper db = DatabaseHelper._();
   static Database? _database;
 
   Future<Database?> get database async {
@@ -12,7 +13,46 @@ class MatjaryDatabaseHelper {
     return _database;
   }
 
-  initDb() async {
-    String path = join(await getDatabasesPath(), "MegaStoreDatabase.db");
+  Future<Database> initDb() async {
+    String path = join(await getDatabasesPath(), appDatabaseName);
+    return await openDatabase(
+      path,
+      version: 1,
+      onCreate: (Database db, int version) async {
+        createTableQueries.map((query) async => await db.execute(query));
+      },
+    );
+  }
+
+  Future<dynamic> getData(tableName) async {
+    var dbClient = await database;
+    List<dynamic> maps =
+        await dbClient?.query(tableName) as List<Map<dynamic, dynamic>>;
+    print(maps);
+    return maps;
+  }
+
+  Future<void> insert(table, row) async {
+    var dbClient = await database;
+    try {
+      await dbClient?.insert(
+        table,
+        row,
+        conflictAlgorithm: ConflictAlgorithm.replace,
+      );
+      print('Inserted');
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  Future<void> delete(table, id) async {
+    var dbClient = await database;
+    try {
+      dbClient?.delete(table, where: 'id=?', whereArgs: [id]);
+      print('Deleted');
+    } catch (e) {
+      print(e);
+    }
   }
 }
