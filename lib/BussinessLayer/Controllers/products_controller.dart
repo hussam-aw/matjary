089c4400b734346d38af400f5a29ac2ec6028060
AliAlsaeed176 <alialsaeed176@gmail.com>
2023-10-05 08:44:35 -0700

    Add backup products method

--- lib/BussinessLayer/Controllers/products_controller.dart
+++ lib/BussinessLayer/Controllers/products_controller.dart
@@ -1,4 +1,7 @@
 import 'package:get/get.dart';
+import 'package:matjary/BussinessLayer/Controllers/connectivity_controller.dart';
+import 'package:matjary/BussinessLayer/helpers/database_helper.dart';
+import 'package:matjary/Constants/app_strings.dart';
 import 'package:matjary/DataAccesslayer/Models/product.dart';
 import 'package:matjary/DataAccesslayer/Repositories/products_repo.dart';
 
@@ -6,11 +9,24 @@ class ProductsController extends GetxController {
   PrdouctsRepo prdouctsRepo = PrdouctsRepo();
   List<Product> products = [];
   var isLoadingProducts = false.obs;
+  final connectivityController = Get.find<ConnectivityController>();
+  DatabaseHelper databaseHelper = DatabaseHelper.db;
 
   Future<void> getProducts() async {
     isLoadingProducts.value = true;
-    products = await prdouctsRepo.getProducts();
+    products =
+        await prdouctsRepo.getProducts(connectivityController.isConnected);
+    backupProducts();
     isLoadingProducts.value = false;
+  }
+
+  Future<void> backupProducts() async {
+    databaseHelper.clearTable(productsTableName);
+    if (connectivityController.isConnected) {
+      for (var product in products) {
+        await databaseHelper.insert(productsTableName, product.toMap());
+      }
+    }
   }
 
   Product? getProductFromId(productId) {
