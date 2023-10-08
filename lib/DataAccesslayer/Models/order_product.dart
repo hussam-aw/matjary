86468aaebbc11c0d3961f035d0b1968af0fc9097
AliAlsaeed176 <alialsaeed176@gmail.com>
2023-10-08 07:31:43 -0700

    Add from database json method and update model

--- lib/DataAccesslayer/Models/order_product.dart
+++ lib/DataAccesslayer/Models/order_product.dart
@@ -1,25 +1,32 @@
 class OrderProduct {
-  int id;
+  int? id;
   int productId;
   int quantity;
   num price;
-  num totalPrice;
-  int orderId;
-  DateTime createdAt;
-  DateTime updatedAt;
+  num? totalPrice;
+  int? orderId;
+  DateTime? createdAt;
+  DateTime? updatedAt;
 
   OrderProduct({
-    required this.id,
+    this.id,
     required this.productId,
     required this.quantity,
     required this.price,
-    required this.totalPrice,
-    required this.orderId,
-    required this.createdAt,
-    required this.updatedAt,
+    this.totalPrice,
+    this.orderId,
+    this.createdAt,
+    this.updatedAt,
   });
 
-  factory OrderProduct.fromJson(Map<String, dynamic> json) => OrderProduct(
+  factory OrderProduct.fromDatabaseJson(Map<String, dynamic> json) =>
+      OrderProduct(
+        productId: json["product_id"],
+        quantity: json["quantity"],
+        price: json["price"],
+      );
+
+  factory OrderProduct.fromApiJson(Map<String, dynamic> json) => OrderProduct(
         id: json["id"],
         productId: json["product_id"],
         quantity: json["quantity"],
@@ -31,13 +38,8 @@ class OrderProduct {
       );
 
   Map<String, dynamic> toJson() => {
-        "id": id,
         "product_id": productId,
         "quantity": quantity,
         "price": price,
-        "total_price": totalPrice,
-        "order_id": orderId,
-        "created_at": createdAt.toIso8601String(),
-        "updated_at": updatedAt.toIso8601String(),
       };
 }
