b89fc3b8b9501d4a629114143b7e934237ebab29
AliAlsaeed176 <alialsaeed176@gmail.com>
2023-10-11 09:22:04 -0700

    Use sync offline methods

--- lib/BussinessLayer/Controllers/home_controller.dart
+++ lib/BussinessLayer/Controllers/home_controller.dart
@@ -6,9 +6,12 @@ import 'package:matjary/BussinessLayer/Controllers/earns_expenses_controller.dar
 import 'package:matjary/BussinessLayer/Controllers/orders_controller.dart';
 import 'package:matjary/BussinessLayer/Controllers/payments_controller.dart';
 import 'package:matjary/BussinessLayer/Controllers/products_controller.dart';
+import 'package:matjary/BussinessLayer/Controllers/statements_controller.dart';
 import 'package:matjary/BussinessLayer/Controllers/store_settings_controller.dart';
+import 'package:matjary/BussinessLayer/Controllers/user_controller.dart';
 import 'package:matjary/BussinessLayer/Controllers/users_controller.dart';
 import 'package:matjary/BussinessLayer/Controllers/wares_controller.dart';
+import 'package:matjary/main.dart';
 
 class HomeController extends GetxController {
   AccountsController accountsController = Get.put(AccountsController());
@@ -16,28 +19,35 @@ class HomeController extends GetxController {
   OrdersController ordersController = Get.put(OrdersController());
   WaresController waresController = Get.put(WaresController());
   ProductsController productsController = Get.put(ProductsController());
+  StatementsController statementsController = Get.put(StatementsController());
   PaymentsController paymentsController = Get.put(PaymentsController());
   EarnsExpensesController earnsExpensesController =
       Get.put(EarnsExpensesController());
   UsersController usersController = Get.put(UsersController());
   StoreSettingsController storeSettingsController =
       Get.put(StoreSettingsController());
-  final connectivityController = Get.find<ConnectivityController>();
+  UserController userController = Get.put(UserController());
+  ConnectivityController connectivityController =
+      Get.find<ConnectivityController>();
 
   void fetchData() async {
     await accountsController.getCachAmount();
-    await accountsController.getAccounts();
     await ordersController.getOrders();
+    await accountsController.getAccounts();
     storeSettingsController.getStoreSettings();
+    if (connectivityController.isConnected) {
+      await ordersController.syncOfflineOrders();
+      await paymentsController.syncOfflinePayments();
+      await statementsController.syncOfflineStatements();
+      await earnsExpensesController.syncOfflineEarnsAndExpenses();
+      earnsExpensesController.getEarnsAndExpenses();
+      paymentsController.getPayments();
+      categoriesController.getCategories();
+      userController.updateUserData(MyApp.appUser!.id);
+      usersController.getUsers();
+    }
     productsController.getProducts();
     waresController.getWares();
-    if (connectivityController.isConnected) {
-      categoriesController.getCategories();
-      paymentsController.getPayments();
-      earnsExpensesController.getStatements();
-      usersController.getUsers();
-      await ordersController.syncOfflineOrders();
-    }
   }
 
   @override
