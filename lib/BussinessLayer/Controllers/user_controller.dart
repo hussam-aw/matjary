4cacda4ccbdc6754cb72043c249b89b5bad33a67
AliAlsaeed176 <alialsaeed176@gmail.com>
2023-10-11 09:12:36 -0700

    Add connectivity check and create update user data method

--- lib/BussinessLayer/Controllers/user_controller.dart
+++ lib/BussinessLayer/Controllers/user_controller.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/widgets.dart';
 import 'package:get/get.dart';
+import 'package:matjary/BussinessLayer/Controllers/connectivity_controller.dart';
 import 'package:matjary/BussinessLayer/Controllers/users_controller.dart';
+import 'package:matjary/DataAccesslayer/Clients/box_client.dart';
 import 'package:matjary/DataAccesslayer/Models/user.dart';
 import 'package:matjary/DataAccesslayer/Repositories/user_repo.dart';
 import 'package:matjary/PresentationLayer/Widgets/snackbars.dart';
+import 'package:matjary/main.dart';
 
 class UserController extends GetxController {
   int? userType = 0;
@@ -15,6 +18,8 @@ class UserController extends GetxController {
   UserRepo userRepo = UserRepo();
   var isLoading = false.obs;
   var savingState = false;
+  final connectivityController = Get.find<ConnectivityController>();
+  BoxClient boxClient = BoxClient();
 
   Map<String, int> counterUserTypes = {
     'مسؤول': 0,
@@ -88,25 +93,37 @@ class UserController extends GetxController {
     String mobilePhone = getMobilePhone();
     String password = getPassword();
     if (userName.isNotEmpty && mobilePhone.isNotEmpty && password.isNotEmpty) {
-      isLoading.value = true;
-      var user = await userRepo.createUser(
-        userType,
-        userName,
-        mobilePhone,
-        password,
-        notifiable,
-      );
-      isLoading.value = false;
-      if (user != null) {
-        usersController.getUsers();
-        savingState = true;
-        setUserDetails(null);
-        SnackBars.showSuccess('تم انشاء المستخدم');
+      if (connectivityController.isConnected) {
+        isLoading.value = true;
+        var user = await userRepo.createUser(
+          userType,
+          userName,
+          mobilePhone,
+          password,
+          notifiable,
+        );
+        isLoading.value = false;
+        if (user != null) {
+          usersController.getUsers();
+          savingState = true;
+          setUserDetails(null);
+          SnackBars.showSuccess('تم انشاء المستخدم');
+        } else {
+          SnackBars.showError('فشل انشاء المستخدم');
+        }
       } else {
-        SnackBars.showError('فشل انشاء المستخدم');
+        SnackBars.showError('لا يوجد اتصال بالانترنت');
       }
     } else {
       SnackBars.showWarning('يرجى تعبئة الحقول المطلوبة');
+    }
+  }
+
+  Future<void> updateUserData(id) async {
+    User? user = await userRepo.getUserDataById(id);
+    if (user != null) {
+      MyApp.appUser = user;
+      await boxClient.setAuthedUser(user);
     }
   }
 }
