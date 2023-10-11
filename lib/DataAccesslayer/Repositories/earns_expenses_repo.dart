789b08b6be64c552d68460a4a497c1a7b41486e5
AliAlsaeed176 <alialsaeed176@gmail.com>
2023-10-11 09:19:25 -0700

    Update methods and add get offline earns expenses method

--- lib/DataAccesslayer/Repositories/earns_expenses_repo.dart
+++ lib/DataAccesslayer/Repositories/earns_expenses_repo.dart
@@ -1,13 +1,13 @@
 import 'dart:convert';
-
-import 'package:matjary/DataAccesslayer/Clients/earns_expenses_client.dart';
+import 'package:matjary/DataAccesslayer/Clients/statement_client.dart';
 import 'package:matjary/DataAccesslayer/Models/statement_with_type.dart';
+import 'package:matjary/main.dart';
 
 class EarnsExpensesRepo {
-  EarnsExpensesClient client = EarnsExpensesClient();
-  Future<List<StatementWithType>> getStatements() async {
-    var response = await client.getStatements();
+  StatementClinet client = StatementClinet();
 
+  Future<List<StatementWithType>> getEarnsAndExpenses() async {
+    var response = await client.getEarnsAndExpenses();
     if (response != "") {
       final parsed = json.decode(response).cast<Map<String, dynamic>>();
       return parsed
@@ -17,31 +17,66 @@ class EarnsExpensesRepo {
     return [];
   }
 
-  Future<bool?> createStatementBsedOnType(
-      type, statement, amount, bankId, date) async {
-    var createdStatement = await client.createStatementBasedOnType(
-        type, statement, amount, bankId, date);
-    if (createdStatement != null) {
-      return true;
+  Future<List<StatementWithType>> getOfflineEarnsAndExpenses() async {
+    var response =
+        await client.getOfflineStatementsBasedOnType(['revenues', 'expenses']);
+    if (response != "") {
+      var parsed = response;
+      return parsed
+          .map<StatementWithType>(
+              (json) => StatementWithType.fromDatabaseMap(json))
+          .toList();
     }
-    return null;
+    return [];
+  }
+
+  Future<bool> createStatementBsedOnType(
+      connected, type, statement, amount, bankId, date) async {
+    Map<String, Object?> statementFieldsMap = {};
+    if (connected) {
+      statementFieldsMap = {
+        "type": type,
+        "statement": statement,
+        "amount": amount,
+        "bank_id": bankId,
+        "date": date,
+        "user_id": MyApp.appUser!.companyId,
+      };
+    } else {
+      statementFieldsMap = {
+        "type": type,
+        "statement": statement,
+        "amount": amount,
+        "other_side": bankId,
+        "created_at": date,
+        "user_id": MyApp.appUser!.companyId,
+      };
+    }
+    var isCreatedStatement =
+        await client.createStatement(connected, statementFieldsMap);
+    return isCreatedStatement;
   }
 
   Future<bool?> updateStatement(
       id, type, statement, amount, bankId, date) async {
-    var updatedStatement =
-        await client.updateStatement(id, type, statement, amount, bankId, date);
-    if (updatedStatement != null) {
-      return true;
-    }
-    return null;
+    var statementFieldMap = {
+      "type": type,
+      "statement": statement,
+      "amount": amount,
+      "bank_id": bankId,
+      "date": date,
+      "user_id": MyApp.appUser!.companyId,
+    };
+    var isUpdatedStatement =
+        await client.updateStatement(id, statementFieldMap);
+    return isUpdatedStatement;
   }
 
-  Future<bool?> deleteStatement(id) async {
+  Future<bool> deleteStatement(id) async {
     var deletedStatement = await client.deleteStatement(id);
     if (deletedStatement != null) {
       return true;
     }
-    return null;
+    return false;
   }
 }
