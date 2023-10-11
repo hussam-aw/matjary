570b1f7f3a7a893a1afb2af11f09f644b02b1de0
AliAlsaeed176 <alialsaeed176@gmail.com>
2023-10-11 09:13:23 -0700

    Replace earns expenses and payments clients with statement client and refactor methods code

--- lib/DataAccesslayer/Clients/statement_client.dart
+++ lib/DataAccesslayer/Clients/statement_client.dart
@@ -1,13 +1,16 @@
 import 'dart:convert';
-
 import 'package:http/http.dart' as http;
+import 'package:matjary/BussinessLayer/helpers/database_helper.dart';
 import 'package:matjary/Constants/api_links.dart';
+import 'package:matjary/Constants/app_strings.dart';
 import 'package:matjary/main.dart';
 
 class StatementClinet {
-  Future<dynamic> getStatements(id) async {
-    var response = await http.get(Uri.parse("$baseUrl$statementsLink/$id"));
+  DatabaseHelper databaseHelper = DatabaseHelper.db;
 
+  Future<dynamic> getStatements() async {
+    var response = await http
+        .get(Uri.parse("$baseUrl$statementsLink/${MyApp.appUser!.id}"));
     if (response.statusCode == 200) {
       return response.body;
     } else {
@@ -15,26 +18,75 @@ class StatementClinet {
     }
   }
 
-  Future<dynamic> createStatement(
-      firstSideId, secondSideId, statement, amount, date) async {
-    var response = await http.post(Uri.parse('$baseUrl$statementLink'),
-        body: jsonEncode(<String, dynamic>{
-          "first_side_id": firstSideId,
-          "second_side_id": secondSideId,
-          "user_id": MyApp.appUser!.id,
-          'type': 'other',
-          "statement": statement,
-          "amount": amount,
-          "date": date,
-        }),
+  Future<dynamic> getPayments() async {
+    var response =
+        await http.get(Uri.parse("$baseUrl$paymentsLink/${MyApp.appUser!.id}"));
+    if (response.statusCode == 200) {
+      return response.body;
+    } else {
+      return "";
+    }
+  }
+
+  Future<dynamic> getEarnsAndExpenses() async {
+    var response = await http.get(
+        Uri.parse("$baseUrl$earnsExpensesLink/${MyApp.appUser!.companyId}"));
+    if (response.statusCode == 200) {
+      return response.body;
+    } else {
+      return "";
+    }
+  }
+
+  Future<dynamic> getOfflineStatementsBasedOnType(types) async {
+    var data = await databaseHelper.getData(
+        offlineTransactionTableName, 'type', types);
+    if (data.isNotEmpty) {
+      await databaseHelper.delete(offlineTransactionTableName, 'type', types);
+      return data;
+    } else {
+      return "";
+    }
+  }
+
+  Future<dynamic> createStatement(connected, map) async {
+    if (connected) {
+      var response = await http.post(Uri.parse('$baseUrl$statementLink'),
+          body: jsonEncode(map),
+          headers: <String, String>{
+            'Content-Type': 'application/json; charset=UTF-8',
+          });
+      if (response.statusCode == 201) {
+        return true;
+      } else {
+        return false;
+      }
+    } else {
+      var isInserted =
+          await databaseHelper.insert(offlineTransactionTableName, map);
+      return isInserted;
+    }
+  }
+
+  Future<dynamic> updateStatement(id, map) async {
+    var response = await http.post(Uri.parse('$baseUrl$statementLink/$id'),
+        body: jsonEncode(map),
         headers: <String, String>{
           'Content-Type': 'application/json; charset=UTF-8',
         });
-
     if (response.statusCode == 201) {
-      return response.body;
+      return true;
     } else {
-      return null;
+      return false;
+    }
+  }
+
+  Future<dynamic> deleteStatement(id) async {
+    var response = await http.delete(Uri.parse('$baseUrl$statementLink/$id'));
+    if (response.statusCode == 201) {
+      return true;
+    } else {
+      return false;
     }
   }
 }
