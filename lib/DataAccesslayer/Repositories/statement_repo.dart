9c5060d015e03c956dfa2d0dbd33bde81271b8f0
AliAlsaeed176 <alialsaeed176@gmail.com>
2023-10-11 09:14:46 -0700

    Update create statement method for offline and online modes and add get offline statements method

--- lib/DataAccesslayer/Repositories/statement_repo.dart
+++ lib/DataAccesslayer/Repositories/statement_repo.dart
@@ -7,8 +7,7 @@ class StatementRepo {
   StatementClinet client = StatementClinet();
 
   Future<List<Statement>> getStatements() async {
-    var response = await client.getStatements(MyApp.appUser!.id);
-
+    var response = await client.getStatements();
     if (response != "") {
       final parsed = json.decode(response).cast<Map<String, dynamic>>();
       return parsed.map<Statement>((json) => Statement.fromMap(json)).toList();
@@ -16,13 +15,41 @@ class StatementRepo {
     return [];
   }
 
-  Future<Statement?> createStatement(
-      firstSideId, secondSideId, statement, amount, date) async {
-    var createdStatement = await client.createStatement(
-        firstSideId, secondSideId, statement, amount, date);
-    if (createdStatement != null) {
-      return Statement.fromMap(jsonDecode(createdStatement));
+  Future<List<Statement>> getOfflineStatements() async {
+    var response = await client.getOfflineStatementsBasedOnType(['other']);
+    if (response != "") {
+      var parsed = response;
+      return parsed.map<Statement>((json) => Statement.fromMap(json)).toList();
     }
-    return null;
+    return [];
+  }
+
+  Future<bool> createStatement(
+      connected, firstSideId, secondSideId, statement, amount, date) async {
+    Map<String, Object?> statementFieldsMap = {};
+    if (connected) {
+      statementFieldsMap = {
+        "first_side_id": firstSideId,
+        "second_side_id": secondSideId,
+        "user_id": MyApp.appUser!.id,
+        'type': 'other',
+        "statement": statement,
+        "amount": amount,
+        "date": date,
+      };
+    } else {
+      statementFieldsMap = {
+        "first_side": firstSideId,
+        "other_side": secondSideId,
+        "user_id": MyApp.appUser!.id,
+        'type': 'other',
+        "statement": statement,
+        "amount": amount,
+        "created_at": date,
+      };
+    }
+    var isStatementCreated =
+        await client.createStatement(connected, statementFieldsMap);
+    return isStatementCreated;
   }
 }
